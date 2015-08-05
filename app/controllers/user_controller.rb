3e282956e78c9f68109a18ab58ff3edbd2693bfa
Joao Pereira <joaopapereira@gmail.com>
2015-08-05 11:16:14 -0400

    Refactor the user controller and added changes to edit form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,18 +1,34 @@
 class UserController < ApplicationController
   def new
-    case request.original_fullpath
-      when /jobseeker/
-        @user = JobSeeker.new
-        @url = jobseeker_index_path
-    end
+    getUser
   end
 
   def create
+    getUser
+    begin
+      @user = @userClass.new params[:user]
+      @user.save!
+    rescue
+      puts "Error #{$!}"
+      render :new
+      return
+    end
+    redirect_to root_path
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+  def getUser
+    case request.original_fullpath
+      when /jobseeker/
+        @user = JobSeeker.new
+        @url = jobseeker_index_path
+        @userClass = JobSeeker
+    end
   end
 end
