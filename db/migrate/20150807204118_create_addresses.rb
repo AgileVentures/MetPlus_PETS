94b87f87907ffa293d86910801ee6005947b6ee7
Joao Pereira <joaopapereira@gmail.com>
2015-08-07 17:26:19 -0400

    Changed address to be mutable and added tests and states

--- db/migrate/20150807204118_create_addresses.rb
+++ db/migrate/20150807204118_create_addresses.rb
@@ -1,6 +1,8 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
+      t.integer :addressable_id
+      t.string :addressable_type
       t.string :full_address
       t.integer :zipcode
       t.string :state
