c916d5cf192b1b6961c99d110306d8e090421797
Joao Pereira <joaopapereira@gmail.com>
2015-08-17 10:50:51 -0400

    Added tests for session helper

--- spec/helpers/session_helper_spec.rb
+++ spec/helpers/session_helper_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the SessionHelper. For example:
-#
-# describe SessionHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe SessionHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {FactoryGirl.create(:user)}
+  describe '#current_user' do
+    it {expect(current_user).to be nil}
+    it {
+      session[:user_id] = user.id
+      expect(current_user).to eq user
+    }
+  end
+  describe '#logged_in?' do
+    it {expect(logged_in?).to be false}
+    it {
+      session[:user_id] = user.id
+      expect(logged_in?).to eq true
+    }
+  end
 end
