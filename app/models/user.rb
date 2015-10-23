06e0b981cf6691cc62ee0a2aada134c73a348106
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-10-23 15:46:00 -0300

    Updates for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
    has_one :pets_account
    actable
-   validates_presence_of :email
+   validates:email,:email => true,
+            :uniqueness => true,
+            :presence => true
    validates_presence_of :first_name
    validates_presence_of :last_name
-   validates_format_of   :phone, 
-    with: /\A\d{3}-\d{3}-\d{4}|\A\d[0-9]\z/,
+   validates   :phone, :phone => true
     
-      message: 'should be in form of 123-345-7989',
-    allow_blank: true
 end
