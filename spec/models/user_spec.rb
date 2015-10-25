342d2e90abc01d2e047b081056f6c685f0c8b4f2
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-10-25 15:03:37 -0300

    updates for user specs indentation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,9 +12,10 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column :phone }
     it { is_expected.to have_db_column :actable_id }
     it { is_expected.to have_db_column :actable_type }
-  end
+   end
 
-    describe 'check model restrictions' do 
+   describe 'check model restrictions' do 
+
      describe 'Email check' do
        subject {FactoryGirl.build(:user)}
        it { should validate_uniqueness_of(:email)}
@@ -24,28 +25,31 @@ RSpec.describe User, type: :model do
                     
 
      end
+
      describe 'FirstName check' do
        subject {FactoryGirl.build(:user)}
        it { is_expected.to validate_presence_of :first_name }
        
      end
+
      describe 'LastName check' do
        subject {FactoryGirl.build(:user)}
        it { is_expected.to validate_presence_of :last_name }
        
      end
-    describe 'Phone' do
+
+     describe 'Phone check' do
        subject {FactoryGirl.build(:user)}
-         it { should_not allow_value('asd', '123456', '123 123 12345', '123    1231 1234', '1123 123 1234', ' 123 123 1234')
-.for(:phone)}
-it { should allow_value('+1 123 123 1234', '123 123 1234', '(123) 123 1234', '1231231234', '+1 (123) 1231234')
-.for(:phone)}
-end
+       it { should_not allow_value('asd', '123456', '123 123 12345', '123 1231 1234', '1123 123 1234', ' 123 123 1234').for(:phone)}
+
+       it { should allow_value('+1 123 123 1234', '123 123 1234', '(123) 123 1234', '1231231234', '+1 (123) 1231234').for(:phone)}
+
+     end
        
 
        
      
- end
+   end
 
    
     
