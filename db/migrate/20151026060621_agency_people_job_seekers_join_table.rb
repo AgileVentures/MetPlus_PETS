bbf629ab0c0d55beca9d9e49ea5d86c44d4c6132
Salem Amba <salemamba@gmail.com>
2015-10-27 09:57:17 -0700

    changed table name to seeker_agency_people

--- db/migrate/20151026060621_agency_people_job_seekers_join_table.rb
+++ db/migrate/20151026060621_agency_people_job_seekers_join_table.rb
@@ -1,9 +1,9 @@
 class AgencyPeopleJobSeekersJoinTable < ActiveRecord::Migration
   	def change
-    	create_table :agencies_seekers, id: false do |t|
+    	create_table :seekers_agency_people, id: false do |t|
     		t.integer :agency_person_id 
     		t.integer :job_seeker_id 
     end
-   	 add_index :agencies_seekers, [:agency_person_id, :job_seeker_id]
+   	 # add_index :seekers_agency_people, [:agency_person_id, :job_seeker_id]
   end
 end
