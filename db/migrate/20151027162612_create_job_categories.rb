6df721319209309c6064980f23249ae435c594d8
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-10-27 20:03:12 -0300

    updates for relationships

--- db/migrate/20151027162612_create_job_categories.rb
+++ db/migrate/20151027162612_create_job_categories.rb
@@ -3,8 +3,10 @@ class CreateJobCategories < ActiveRecord::Migration
     create_table :job_categories do |t|
       t.string :name
       t.string :description
-      t.references :skills, index: true, foreign_key: true 
       t.references :jobs, index: true, foreign_key: true
+      t.references :skills, index: true, foreign_key: true
+
+      t.references :job_speciliaties, index: true, foreign_key: true
 
       t.timestamps null: false
     end
