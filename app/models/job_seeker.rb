7ccfe6a533bcd838561515c1930f7c2c8c74dabc
Salem Amba <salemamba@gmail.com>
2015-10-26 18:32:02 -0700

    Added YearValidator class to validators

--- app/models/job_seeker.rb
+++ app/models/job_seeker.rb
@@ -5,21 +5,8 @@ class JobSeeker < ActiveRecord::Base
   has_one	   :address, as: :location
   has_and_belongs_to_many :agency_people, :join_table => "agencies_seekers" 
   validates_presence_of :year_of_birth, :resume
-  YEARS = 100 
-  validate :validate_year_of_birth  
+  validate :year_of_birth  
 
 
-  private 
-  	def validate_year_of_birth
-
-  	 self.year_of_birth =~/\A\d{4}\z/ ? year=self.year_of_birth :
-  	         errors.add(:year_of_birth, "cannot be more than or less 4 digits")
-  	         
-  	  if year 
-  	  	 (year.to_i<=Date.today.year)&&( year.to_i>=(Date.today.year - YEARS)) ? 
-  	  	         self.year_of_birth = year.to_s : errors.add(:year_of_birth, 
-  	  	         	 "should be between #{Date.today.year}-#{Date.today.year-YEARS}")
-  	  end
-      
-  	end
+  
 end
---
+++ app/validators/year_validator.rb
@@ -0,0 +1,9 @@
+class YearValidator < ActiveModel::EachValidator
+  def validate_each(object, attribute, value)
+    return if value == nil
+    reg = /\A\d{4}\z/
+    
+    if !value.match reg && !(value.to_i<=Date.today.year)&&( value.to_i>=(Date.today.year - 100))
+        object.errors[attribute] << (options[:message] || 'incorrect format')
+    end
+end
