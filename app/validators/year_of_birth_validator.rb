dd69fc136e5980f40a68a3a051db6f5c0cfb8bc8
Salem Amba <salemamba@gmail.com>
2015-10-27 15:55:02 -0700

    updated validator folder

--- app/validators/year_of_birth_validator.rb
+++ app/validators/year_of_birth_validator.rb
@@ -1,9 +1,9 @@
-class YearValidator < ActiveModel::EachValidator
+class YearOfBirthValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     return if value == nil
     reg = /\A\d{4}\z/
     
-    if !value.match reg && !((value.to_i<=Date.today.year)&&( value.to_i>=(Date.today.year - 100)))
+    if value=~reg && !((1915..2015).member?(value.to_i)))
         object.errors[attribute] << (options[:message] || 'incorrect format')
     end
 end
