b0c487acce765767be4083bc60bdb1a12ddb7399
Salem Amba <salemamba@gmail.com>
2015-10-27 13:30:38 -0700

    fixed large characters error joint AP-JS join table

--- db/migrate/20151026060621_agency_people_job_seekers_join_table.rb
+++ db/migrate/20151026060621_agency_people_job_seekers_join_table.rb
@@ -1,10 +1,9 @@
 class AgencyPeopleJobSeekersJoinTable < ActiveRecord::Migration
   	def change
-    	create_table :seekers_agency_people, id: false do |t|
+    	create_table :agency_people_job_seekers, id: false do |t|
     		t.integer :agency_person_id 
     		t.integer :job_seeker_id 
     end
-   	  add_index :seekers_agency_people, :job_seeker_id
-   	  add_index :seekers_agency_people, :agency_person_id
+   	  add_index :agency_people_job_seekers, [:agency_person_id, :job_seeker_id], :name => "seekers_agency_people"
   end
 end
