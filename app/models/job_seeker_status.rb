0828a1f0b3386bf24bd8254905f46fe3647976d1
Salem Amba <salemamba@gmail.com>
2015-10-27 21:06:21 -0700

    deleted job seeker status model/unwanted here.

--- app/models/job_seeker_status.rb
+++
@@ -1,7 +0,0 @@
-class JobSeekerStatus < ActiveRecord::Base
-	has_many :jobseekers, class_name: "JobSeeker" 
-	validates_presence_of :description, :value 
-	validates_length_of :description, within: 10..255, 
-				too_long:  "is too long (maximum is 255 characters)",
-				too_short: "is too short (minimum is 10 characters)" 
-end
