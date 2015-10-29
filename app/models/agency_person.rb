142681a7affc4597a8183092e0634b7c960ed3bc
Salem Amba <salemamba@gmail.com>
2015-10-28 19:20:52 -0700

    updated AP association relation.

--- app/models/agency_person.rb
+++ app/models/agency_person.rb
@@ -1,5 +1,11 @@
 class AgencyPerson < ActiveRecord::Base
+  acts_as :user 
+  has_many :job_specialities
+  has_many :job_categories, :through => :job_specialities
   belongs_to :address
   belongs_to :agency
-  has_and_belongs_to_many :jobseekers, class_name: "JobSeeker"
+  has_and_belongs_to_many :job_seekers
+  has_and_belongs_to_many :agency_roles
+
+  
 end
--- spec/models/agency_person_spec.rb
+++ spec/models/agency_person_spec.rb
@@ -6,14 +6,28 @@ describe AgencyPerson, type: :model do
 	    it { is_expected.to have_db_column :agency_id  }
 	    it { is_expected.to have_db_column :address_id }
 	end
-
+     
 	xit "remove xit part after merging with Agency model" do 
 		it{ is_expected.to belong_to(:agency) } 
 	end
+    xit "remove xit part after merging or creating JobSpeciality model" do 
+		it{ is_expected.to have_many :job_specialities}
+	end
 
-	xit "remove xit part after merging with JobSeeker model" do 
-		it{ is_expected.to have_and_belong_to_many(:jobseekers) } 
+	xit "remove xit part after merging JobSeeker model" do 
+		it{ is_expected.to have_and_belong_to_many(:job_seekers) }
+		
+
+	end
+
+	xit "remove xit part after mergin or creating AgencyRole model " do 
+		it{ is_expected.to have_and_belong_to_many(:agency_roles) }
+	end
+
+	xit "remove xit part after mergin or creating JobCategory model " do 
+		it{ is_expected.to have_many(:job_categories) }
 	end
 
 	it{ is_expected.to belong_to(:address) }
+
 end
