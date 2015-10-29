c721c79b22756f32b94c8b84c670ae207282addf
Salem Amba <salemamba@gmail.com>
2015-10-28 19:29:02 -0700

    Added acts_as test for AP model.

--- app/models/agency_person.rb
+++ app/models/agency_person.rb
@@ -1,9 +1,11 @@
 class AgencyPerson < ActiveRecord::Base
-  acts_as :user 
-  has_many :job_specialities
-  has_many :job_categories, :through => :job_specialities
-  belongs_to :address
-  belongs_to :agency
+	
+  acts_as     :user 
+  belongs_to  :address
+  belongs_to  :agency
+  has_many    :job_specialities
+  has_many    :job_categories, 
+              :through => :job_specialities
   has_and_belongs_to_many :job_seekers
   has_and_belongs_to_many :agency_roles
 
--- spec/models/agency_person_spec.rb
+++ spec/models/agency_person_spec.rb
@@ -10,14 +10,13 @@ describe AgencyPerson, type: :model do
 	xit "remove xit part after merging with Agency model" do 
 		it{ is_expected.to belong_to(:agency) } 
 	end
+
     xit "remove xit part after merging or creating JobSpeciality model" do 
 		it{ is_expected.to have_many :job_specialities}
 	end
 
 	xit "remove xit part after merging JobSeeker model" do 
 		it{ is_expected.to have_and_belong_to_many(:job_seekers) }
-		
-
 	end
 
 	xit "remove xit part after mergin or creating AgencyRole model " do 
@@ -29,5 +28,18 @@ describe AgencyPerson, type: :model do
 	end
 
 	it{ is_expected.to belong_to(:address) }
+
+
+	context "#acting_as?" do
+	    it "returns true for supermodel class and name" do
+	      expect(AgencyPerson.acting_as? :user).to be true
+	      expect(AgencyPerson.acting_as? User).to  be true
+	    end
+
+	    it "returns false for anything other than supermodel" do
+	      expect(AgencyPerson.acting_as? :model).to be false
+	      expect(AgencyPerson.acting_as? String).to be false
+	    end
+	end
 
 end
