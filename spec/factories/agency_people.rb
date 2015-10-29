0859c867a6ddb50f26ba603711fac7cb615da428
Salem Amba <salemamba@gmail.com>
2015-10-29 14:13:15 -0700

    reset the issue-#70

--- spec/factories/agency_people.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :agency_person do
-    address nil
-    agency nil
-  end
-
-end
---
+++ spec/factories/job_seeker_statuses.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :job_seeker_status do
+    value "MyString"
+description "MyText"
+  end
+
+end
