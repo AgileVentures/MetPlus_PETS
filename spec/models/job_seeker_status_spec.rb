8789f3cde86bda3e71e06795c09b4579cc6b4014
Salem Amba <salemamba@gmail.com>
2015-10-29 14:41:05 -0700

    removed pending message in jsk spec file.

--- spec/models/job_seeker_status_spec.rb
+++ spec/models/job_seeker_status_spec.rb
@@ -14,12 +14,10 @@ describe JobSeekerStatus, type: :model do
 	it{is_expected.to have_db_column :value} 
 
 
-	it{is_expected.to validate_presence_of :description} 
+	it {is_expected.to validate_presence_of :description} 
 	it { is_expected.to validate_presence_of :value }
-
-	it "uncomment the pending when JS model created", pending: true do 
-		is_expected.to have_many :jobseekers 
-	end
+	it { is_expected.to have_many :jobseekers }
+	
 	
 
 	it{ is_expected.to validate_length_of(:description).is_at_most(255)}
