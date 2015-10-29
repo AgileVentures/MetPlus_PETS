a94d0252758bb5229e4e232b094996f2b4e08fc6
Salem Amba <salemamba@gmail.com>
2015-10-28 17:53:04 -0700

    Updated job seeker model, has_and_belongs_to_many connection with agency person model.

--- app/models/job_seeker.rb
+++ app/models/job_seeker.rb
@@ -3,7 +3,7 @@ class JobSeeker < ActiveRecord::Base
   belongs_to :job_seeker_status
   #has_one    :resume 
   has_one	   :address, as: :location
-  has_and_belongs_to_many :agency_people, :join_table => "seekers_agency_people" 
+  has_and_belongs_to_many :agencypeople, class_name: "AgencyPerson", :join_table => "seekers_agency_people" 
   validates_presence_of :year_of_birth, :resume
   validates  :year_of_birth, :year_of_birth => true   
   
