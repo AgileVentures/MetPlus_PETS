3fc12a1dfb3231a59aebff9e5b77af8ffe37a84f
Salem Amba <salemamba@gmail.com>
2015-11-01 09:02:49 -0800

    updated spec file, company person and fixed conflict on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ActiveRecord::Base
    actable
    validates :email,:email => true,
-<<<<<<< HEAD
              :uniqueness => true,
              :presence => true
-=======
-            :uniqueness => true,
-            :presence => true
->>>>>>> development
    validates_presence_of :first_name
    validates_presence_of :last_name
    validates   :phone, :phone => true
--- spec/models/company_person_spec.rb
+++ spec/models/company_person_spec.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 describe CompanyPerson, type: :model do
    describe 'Database schema' do
-    it { is_expected.to have_db_column :id }
-    it { is_expected.to have_db_column :email}
-    it { is_expected.to have_db_column :first_name }
-    it { is_expected.to have_db_column :last_name }
-    it { is_expected.to have_db_column :phone }
     it { is_expected.to have_db_column :company_id}
     it { is_expected.to have_db_column :address_id}
    end
@@ -14,7 +9,6 @@ describe CompanyPerson, type: :model do
    describe 'check model restrictions' do 
 
      describe 'Email check' do
-       subject {FactoryGirl.build(:company_person)}
        it { should validate_uniqueness_of(:email)}
        it { should validate_presence_of(:email)}
        it { should_not allow_value('abc', 'abc@abc', 'abcdefghjjkll').for(:email)}
@@ -22,19 +16,16 @@ describe CompanyPerson, type: :model do
     end
 
      describe 'FirstName check' do
-       subject {FactoryGirl.build(:company_person)}
        it { is_expected.to validate_presence_of :first_name }
        
      end
 
      describe 'LastName check' do
-       subject {FactoryGirl.build(:company_person)}
        it { is_expected.to validate_presence_of :last_name }
        
      end
 
      describe 'Phone check' do
-       subject {FactoryGirl.build(:company_person)}
        it { should_not allow_value('asd', '123456', '123 123 12345', '123 1231 1234', '1123 123 1234', ' 123 123 1234').for(:phone)}
 
        it { should allow_value('+1 123 123 1234', '123 123 1234', '(123) 123 1234', '1231231234', '+1 (123) 1231234').for(:phone)}
@@ -42,5 +33,17 @@ describe CompanyPerson, type: :model do
      end
        
  end
+
+ context "#acting_as?" do
+        it "returns true for supermodel class and name" do
+          expect(JobSeeker.acting_as? :user).to be true
+          expect(JobSeeker.acting_as? User).to  be true
+        end
+
+        it "returns false for anything other than supermodel" do
+          expect(JobSeeker.acting_as? :model).to be false
+          expect(JobSeeker.acting_as? String).to be false
+        end
+  end
 
 end
