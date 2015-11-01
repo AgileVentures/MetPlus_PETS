d9c4f16b58a2292ead6e7eeb7df2e9a9f9a0025f
Salem Amba <salemamba@gmail.com>
2015-11-01 10:25:20 -0800

    merged and fixed conflict on merge.

--- spec/models/job_seeker_spec.rb
+++ spec/models/job_seeker_spec.rb
@@ -10,10 +10,8 @@ describe JobSeeker, type: :model do
 	
 		it {is_expected.to validate_presence_of(:year_of_birth)} 
 		it {is_expected.to validate_presence_of(:resume)} 
-
-		xit "as moment, do not have has_and_belongs_to_many association to AgencyPerson" do 
-	      it {is_expected.to have_and_belong_to_many :agency_people}
-	    end
+        it {is_expected.to have_and_belong_to_many :agency_people}
+	    
 		
 
 		it{should allow_value('1987', '1916', '2000', '2014').for(:year_of_birth)} 
