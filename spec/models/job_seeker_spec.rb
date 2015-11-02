3bfb2f4a1b34eed96a935e5d2cb6c27dd73e665f
Salem Amba <salemamba@gmail.com>
2015-11-01 20:14:20 -0800

    tryied to update spec and job_seeker model.

--- spec/models/job_seeker_spec.rb
+++ spec/models/job_seeker_spec.rb
@@ -13,13 +13,8 @@ describe JobSeeker, type: :model do
 	
 		it {is_expected.to validate_presence_of(:year_of_birth)} 
 		it {is_expected.to validate_presence_of(:resume)} 
-        #it {is_expected.to have_and_belong_to_many :agency_people}
+        it {is_expected.to have_and_belong_to_many :agency_people}
 	    
-		it  do 
-			should have_and_belong_to_many(:agency_people).join_table('seekers_agency_people')
-		end
-		
-
 		it{should allow_value('1987', '1916', '2000', '2014').for(:year_of_birth)} 
 	    it{should_not allow_value('1911', '899', '1890', 'salem').for(:year_of_birth)}
        
