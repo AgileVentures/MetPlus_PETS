909356373d3d5711e4e82846d6e02920f39a604c
Salem Amba <salemamba@gmail.com>
2015-11-03 15:48:52 -0800

    fixed indeantation ...

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable,:validatable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable, :validatable
+         :recoverable, :rememberable, :trackable, :confirmable,
+         :validatable
    actable
    validates_presence_of :first_name
    validates_presence_of :last_name
