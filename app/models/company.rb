e51db2c4465c065c0176ab6c4c17b629f185a8ac
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-11-03 17:09:36 -0400

    create company model

---
+++ app/models/company.rb
@@ -0,0 +1,12 @@
+class Company < ActiveRecord::Base
+ has_many :company_people
+ has_many :addresses, as: :location
+
+validates_presence_of :name, :ein, :website, :phone, :email
+  validates_length_of   :name, maximum: 100
+  validates_length_of   :website, maximum: 200
+  validates :phone, :phone => true
+  validates :email, :email => true
+  validates :website, :website => true
+  
+end
