53ca8050732a457f34355cb8ad474b353663dbce
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-11-03 14:51:14 -0400

    updates for associations

--- app/models/job_category.rb
+++ app/models/job_category.rb
@@ -1,7 +1,7 @@
 class JobCategory < ActiveRecord::Base
  has_and_belongs_to_many :agency_people, join_table: :job_specialities
  has_many :jobs
- has_and_belongs_to_many:skills
+ has_and_belongs_to_many :skills
 
 
  validates_presence_of :name
