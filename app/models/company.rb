3f14ccc0f2aee020f8120424a18895ee63efb710
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-11-03 17:51:39 -0400

    update for companymodel

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,8 @@
 class Company < ActiveRecord::Base
- has_many :company_people
- has_many :addresses, as: :location
+  has_many :company_people
+  has_many :addresses, as: :location
 
-validates_presence_of :name, :ein, :website, :phone, :email
+  validates_presence_of :name, :ein, :website, :phone, :email
   validates_length_of   :name, maximum: 100
   validates_length_of   :website, maximum: 200
   validates :phone, :phone => true
