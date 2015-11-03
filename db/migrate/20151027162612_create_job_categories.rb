53fb75274d82a6690d01776d7ddea967d5f22e3d
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-11-03 14:52:17 -0400

    updates for db migtation

--- db/migrate/20151027162612_create_job_categories.rb
+++ db/migrate/20151027162612_create_job_categories.rb
@@ -3,11 +3,7 @@ class CreateJobCategories < ActiveRecord::Migration
     create_table :job_categories do |t|
       t.string :name
       t.string :description
-      t.belongs_to :jobs, index: true, foreign_key: true
-      t.belongs_to :skills, index: true, foreign_key: true
-
-      t.belongs_to :job_speciliaties, index: true, foreign_key: true
-
+            
       t.timestamps null: false
     end
   end
