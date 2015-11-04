21dc87e8a31f01537a54dbaae1b4bec853e98236
Salem Amba <salemamba@gmail.com>
2015-11-04 11:20:56 -0800

    fixed  factories file

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -2,13 +2,10 @@ FactoryGirl.define do
   factory :job do
     title "MyString"
     description "MyString"
-    company_id 1
-    company_person_id 1
+    company 
+    company_person 
     address
-    job_category_id 1
+    job_category 
   end
   
-  # replace the id's for 'belongs_to' associations above with actual factory references
-  # as the associated models are defined (company, company_person, job_category)
-
 end
