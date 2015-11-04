07e9cb9416e57fec8909dcdc5a13f404a352a88a
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-11-03 21:20:02 -0400

    updates for indentation

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :company do
     name "Mycompany"
-ein "234567"
-phone "234-678-9876"
-email "Mycompany@mail.com"
-website "Mycompany.com"
+    ein "23-4567789"
+    phone "234-678-9876"
+    email "Mycompany@mail.com"
+    website "Mycompany.com"
   end
 
 end
