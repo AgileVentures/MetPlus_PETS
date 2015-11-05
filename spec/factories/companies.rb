c412d01d3d8af988990fb93c6161367c29df381e
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-11-05 02:50:22 -0400

    updates for factoriescompanies

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :company do
-    name "Mycompany"
-    ein "23-4567789"
-    phone "234-678-9876"
-    email "Mycompany@mail.com"
-    website "Mycompany.com"
+    name   'My company'
+    ein    '23-4567789'
+    phone  '123 123 1234'
+    email   'xyz@company.com'
+    website 'http://www.company.org'
   end
 
 end
