1266d85d3773d7664662956650d12ce2556f84ea
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-11-17 00:33:32 -0400

    updates for functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,53 @@ class User < ActiveRecord::Base
    def self.is_job_seeker?(user)
      user.actable_type == 'JobSeeker'
    end
+
+   def self.is_job_developer?(user)
+      return false unless user.actable_type == "AgencyPerson"
+      person = AgencyPerson.find user.actable_id 
+      person.agency_roles.each do |ar|
+       return true if ar.role == "Job Developer" 
+      end
+   end
+    
+   def self.is_case_manager?(user)
+      return false unless user.actable_type == "AgencyPerson"
+      person = AgencyPerson.find user.actable_id 
+      person.agency_roles.each do |ar|
+       return true if ar.role == "Case Manager" 
+      end
+   end
+ 
+    def self.is_agency_admin?(user)
+      return false unless user.actable_type == "AgencyPerson"
+      person = AgencyPerson.find user.actable_id 
+      person.agency_roles.each do |ar|
+       return true if ar.role == "Agency Admin" 
+      end
+    end
+
+    def self.is_agency_manager?(user)
+      return false unless user.actable_type == "AgencyPerson"
+      person = AgencyPerson.find user.actable_id 
+      person.agency_roles.each do |ar|
+       return true if ar.role == "Agency Manager" 
+      end
+    end
+    
+    def self.is_company_admin?(user)
+      return false unless user.actable_type == "CompanyPerson"
+      person = CompanyPerson.find user.actable_id 
+      person.company_roles.each do |ca|
+       return true if ca.role == "Company Admin" 
+      end
+    end
+    
+    def self.is_employee?(user)
+      return false unless user.actable_type == "CompanyPerson"
+      person = CompanyPerson.find user.actable_id 
+      person.company_roles.each do |ce|
+       return true if ce.role == "Employee" 
+      end
+    end
+
 end
