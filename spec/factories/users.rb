a8f00f4a28627bb7a256a05b4f964b6d63b152d6
Salem Amba <salemamba@gmail.com>
2015-11-18 05:30:12 -0800

    updated factories and  strong parameters.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,18 @@
+
+
 FactoryGirl.define do
+	sequence :email do |n|
+		"unique#{n}@gmail.com"
+    end
+
   factory :user do
 	password 'MyString1233'
 	password_confirmation 'MyString1233'
 	first_name 'John'
 	last_name "Doe"
-	email 'johndoe@place.com'
+	email 
 	phone '(123) 123 1234'
 	confirmed_at Date.today
-
   end
 
 end
