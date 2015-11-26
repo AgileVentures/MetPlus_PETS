46b1ad6f4cf6423368ade7f8e49e635c36de750e
Patrick Bolger <patmbolger@gmail.com>
2015-11-26 06:42:02 -0500

    added agencies controller tests

--- spec/controllers/agencies_controller_spec.rb
+++ spec/controllers/agencies_controller_spec.rb
@@ -19,22 +19,42 @@ RSpec.describe AgenciesController, type: :controller do
   end
 
   describe "PATCH #update" do
-    before(:each) do
-      @agency = FactoryGirl.create(:agency)
-      patch :update, agency: FactoryGirl.attributes_for(:agency),
+    context 'valid attributes' do
+      before(:each) do
+        @agency = FactoryGirl.create(:agency)
+        patch :update, agency: FactoryGirl.attributes_for(:agency),
                      id: @agency
+      end
+      it 'assigns @agency for updating' do
+        expect(assigns(:agency)).to eq @agency
+      end
+      it 'returns redirect status' do
+        expect(response).to have_http_status 302
+      end
+      it 'sets flash message' do
+        expect(flash[:notice]).to eq "Agency was successfully updated."
+      end
+      it 'redirects to agency admin home' do
+        expect(response).to redirect_to(agency_admin_home_path)
+      end
     end
-    it 'assigns @agency for updating' do
-      expect(assigns(:agency)).to eq @agency
-    end
-    it 'returns redirect status' do
-      expect(response).to have_http_status 302
-    end
-    it 'sets flash message' do
-      expect(flash[:notice]).to eq "Agency was successfully updated."
-    end
-    it 'redirects to agency admin home' do
-      expect(response).to redirect_to(agency_admin_home_path)
+    context 'invalid attributes' do
+      before(:each) do
+        @agency = FactoryGirl.create(:agency)
+        @agency.assign_attributes(phone: '', website: 'nodomain')
+        @agency.valid?
+        patch :update, agency: FactoryGirl.attributes_for(:agency, 
+                        phone: '', website: 'nodomain'), id: @agency
+      end
+      it 'assigns @model_errors for error display in layout' do
+        expect(assigns(:model_errors).full_messages).to eq @agency.errors.full_messages
+      end
+      it 'renders edit template' do
+        expect(response).to render_template('edit')
+      end
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
