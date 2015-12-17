e86f71343b17260df752ccb81466437fde0fcbe0
Amy Lightholder <sidhene@gmail.com>
2015-12-17 11:42:11 -0800

    got view working, but no new record yet

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,13 +1,25 @@
 class CompaniesController < ApplicationController
 
   def new
-    @comany = Company.new
-  end
-
-  def create
     @company = Company.new
   end
 
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      flash.notice = "Success!"
+      redirect_to root_path
+    else
+      render 'new'
+      flash.notice = "Oopsie!"
+    end
+  end
+
   private
+
+  def company_params
+   	params.require(:company).permit(:name, :email, :phone,
+   	:website, :ein, :description)
+ 	end
 
 end
