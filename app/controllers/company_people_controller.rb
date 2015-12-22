3a1fa53c794185ef360a39e458abfef0a7df7880
Amy Lightholder <sidhene@gmail.com>
2015-12-22 12:38:08 -0800

    form work

--- app/controllers/company_people_controller.rb
+++ app/controllers/company_people_controller.rb
@@ -1,10 +1,34 @@
 class CompanyPeopleController < ApplicationController
+<<<<<<< HEAD
 
   def new
+=======
+  def new
+    # THIS CODE IS NOT READY FOR TESTING
+>>>>>>> form work
     @company = company.this_company(current_user)
     @company_person = CompanyPerson.new
   end
 
+<<<<<<< HEAD
+=======
+  def create
+    # THIS CODE IS NOT READY FOR TESTING
+    @company = Company.find(params[:company_id])
+    @company_person = CompanyPerson.new
+    @company_person.assign_attributes(company_person_params)
+    @company.company_people << @company_person
+    if @company_person.valid?
+      @company_person.save
+      # need to resolve flash notice in Company and render confirmation
+      # flash[:notice] = "Person was successfully created."
+    else
+      @model_errors = @company_person.errors
+      render :new
+    end
+  end
+
+>>>>>>> form work
   def show
     @company_person = companyPerson.find(params[:id])
   end
@@ -18,6 +42,10 @@ class CompanyPeopleController < ApplicationController
     @company_person.assign_attributes(company_person_params)
 
     if @company_person.save
+<<<<<<< HEAD
+=======
+      #whatif company saves but companyPerson doesn't?
+>>>>>>> form work
     else
       @model_errors = @company_person.errors
       render :edit
@@ -30,7 +58,12 @@ class CompanyPeopleController < ApplicationController
   private
 
   def company_person_params
+<<<<<<< HEAD
     params.require(:company_person).permit(:first_name, :last_name, :phone, :email, :password, :password_confirmation, :company_id,
                           company_role_ids: [])
+=======
+    params.require(:company_person).permit(:first_name, :last_name, :phone, :email
+                          company_role_ids:)
+>>>>>>> form work
   end
 end
