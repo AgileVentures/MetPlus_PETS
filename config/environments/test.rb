b9879dc343d2cce7c8cde7e2147a08b2b0950014
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-12-23 18:35:47 -0400

    added configactionMailermethodfortest

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = { :host => 'localhost' }
+ 
 end
