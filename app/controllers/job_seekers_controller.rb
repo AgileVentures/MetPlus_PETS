121da22c366fd97610c8223f6097d45f61ed18c3
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-12-24 14:54:00 -0400

    create jobseeker controller

--- app/controllers/job_seekers_controller.rb
+++ app/controllers/job_seekers_controller.rb
@@ -7,11 +7,13 @@ class JobSeekersController < ApplicationController
   def create
     @jobseeker = JobSeeker.new(jobseeker_params)
     if @jobseeker.save
+
       flash[:notice] = "A message with a confirmation and link has been sent to your email address. Please follow the link to activate your account."
       redirect_to root_path
     else
       @model_errors = @jobseeker.errors
       render 'new'
+
     end
     
   end
@@ -24,6 +26,7 @@ class JobSeekersController < ApplicationController
  
   def update
     @jobseeker = JobSeeker.find(params[:id])
+
     if @jobseeker.update_attributes(jobseeker_params)
       sign_in :user, @jobseeker.user, bypass: true
       flash[:notice] = "Jobseeker was updated successfully."
