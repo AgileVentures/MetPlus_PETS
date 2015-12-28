7bbc94b3357d5ed12ca36a184c750976c33086ef
vijayakarumudi <vijaya.karumudi@yahoo.com>
2015-12-28 02:58:27 -0400

    updates for routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   resources :job_seekers 
     
    # The priority is based upon order of creation: first created -> highest priority.
+
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
