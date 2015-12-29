5fb27fd16c77594a60edbc6a250290fa88bd1441
Patrick Bolger <patmbolger@gmail.com>
2015-12-29 18:52:54 -0500

    restructure complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,7 +42,7 @@
 
 # User.find_or_create_by(email: 'salemamba@gmail.com') do |user|
 #     user.first_name = "salem"
-#     user.last_name  = 'amba' 
+#     user.last_name  = 'amba'
 #     user.password = 'secret123'
 #     user.password_confirmation = 'secret123'
 #     user.phone ='619-316-8971'
@@ -50,7 +50,7 @@
 # end
 
 
-Job.delete_all 
+Job.delete_all
 Company.delete_all
 
 cp1 = Company.create(:ein => '12-2123244', :phone=> '721-234-4646',  email: 'casemanager@gmail.com',  website: 'http://www.wallmart.com', :name=> 'Walmart')
@@ -69,18 +69,18 @@ Job.create(:title => 'Security Personel', :description => 'If you have Security 
             :company_person_id => '4', :job_category_id => '4' )
 
 #incase rerun comes in. not necessary
-JobSeekerStatus.delete_all 
+JobSeekerStatus.delete_all
 
 ['Unemployedlooking', 'Employedlooking', 'Employednotlooking'].each do |status|
-  case status 
+  case status
 	when 'Unemployedlooking'
-    @jss1 = JobSeekerStatus.find_or_create_by(:value => status, 
+    @jss1 = JobSeekerStatus.find_or_create_by(:value => status,
               description: "A jobseeker Without any work and looking for a job.")
   when 'Employedlooking'
-    @jss2 = JobSeekerStatus.find_or_create_by(:value => status, 
+    @jss2 = JobSeekerStatus.find_or_create_by(:value => status,
               description: "A jobseeker with a job and looking for a job.")
   when 'Employednotlooking'
-    @jss3 = JobSeekerStatus.find_or_create_by(:value => status, 
+    @jss3 = JobSeekerStatus.find_or_create_by(:value => status,
               description: "A jobseeker with a job and not looking for a job for now.")
   end
 end
@@ -88,13 +88,13 @@ end
 #in case of seeding multiple times
 SkillLevel.delete_all
 
-SkillLevel.create(name: 'Beginner', 
+SkillLevel.create(name: 'Beginner',
             description: 'Entry level or minimal proficiency')
-SkillLevel.create(name: 'Intermediate', 
+SkillLevel.create(name: 'Intermediate',
             description: 'Proficient in some aspects, requires supervision')
-SkillLevel.create(name: 'Advanced', 
+SkillLevel.create(name: 'Advanced',
             description: 'Proficient in all aspects, requires little supervision')
-SkillLevel.create(name: 'Expert', 
+SkillLevel.create(name: 'Expert',
             description: 'Proficient in all aspects, able to work indepently')
 
 # Create all agency roles - this should stay in production version of this file
@@ -114,71 +114,82 @@ agency = Agency.create!(name: 'MetPlus', website: 'metplus.org',
           description: 'Michigan Employment & Training Plus, (MET|PLUS) is a 501 (c) 3, Vocational Training non-profit organization that strives to assist Michigan jobseekers with invaluable training and job development that will put them on a career path to success.')
 
 branch = Branch.create(code: '001', agency: agency)
-branch.address = Address.create!(city: 'Detroit', 
+branch.address = Address.create!(city: 'Detroit',
             street: '123 Main Street', zipcode: 48201)
-            
+
 branch = Branch.create(code: '002', agency: agency)
-branch.address = Address.create!(city: 'Detroit', 
+branch.address = Address.create!(city: 'Detroit',
             street: '456 Sullivan Street', zipcode: 48204)
-            
+
 branch = Branch.create(code: '003', agency: agency)
-branch.address = Address.create!(city: 'Detroit', 
+branch.address = Address.create!(city: 'Detroit',
             street: '3 Auto Drive', zipcode: 48206)
 
 # branch without address or people - for testing purposes
 branch = Branch.create(code: '004', agency: agency)
 
-agency_person = AgencyPerson.new(first_name: 'John', last_name: 'Smith', 
-                      agency_id: agency.id, email: 'pets_admin@metplus.org', 
+# Job Seekers
+js1 = JobSeeker.create(first_name: 'Tom', last_name: 'Seeker',
+                      email: 'tom@gmail.com', password: 'qwerty123',
+              year_of_birth: '1980', resume: 'text',
+          job_seeker_status: @jss1, confirmed_at: Time.now)
+
+js2 = JobSeeker.create(first_name: 'Mary', last_name: 'McCaffrey',
+                      email: 'mary@gmail.com', password: 'qwerty123',
+              year_of_birth: '1970', resume: 'text',
+          job_seeker_status: @jss2, confirmed_at: Time.now)
+
+js3 = JobSeeker.create(first_name: 'Frank', last_name: 'Williams',
+                      email: 'frank@gmail.com', password: 'qwerty123',
+              year_of_birth: '1970', resume: 'text',
+          job_seeker_status: @jss3, confirmed_at: Time.now)
+
+# Agency People
+agency_aa = AgencyPerson.new(first_name: 'John', last_name: 'Smith',
+                      agency_id: agency.id, email: 'pets_admin@metplus.org',
                       password: 'qwerty123', confirmed_at: Time.now,
                       branch_id: agency.branches[0].id,
                       status: AgencyPerson::STATUS[:ACT])
-agency_person.agency_roles << AgencyRole.find_by_role(AgencyRole::ROLE[:AA])
-agency_person.save!
+agency_aa.agency_roles << AgencyRole.find_by_role(AgencyRole::ROLE[:AA])
+agency_aa.save!
 
-agency_person = AgencyPerson.new(first_name: 'Chet', last_name: 'Pitts', 
-                      agency_id: agency.id, email: 'chet@metplus.org', 
+agency_cm_and_jd = AgencyPerson.new(first_name: 'Chet', last_name: 'Pitts',
+                      agency_id: agency.id, email: 'chet@metplus.org',
                       password: 'qwerty123', confirmed_at: Time.now,
                       branch_id: agency.branches[1].id,
                       status: AgencyPerson::STATUS[:ACT])
-agency_person.agency_roles << AgencyRole.find_by_role(AgencyRole::ROLE[:CM])
-agency_person.agency_roles << AgencyRole.find_by_role(AgencyRole::ROLE[:JD])
-agency_person.save!
+agency_cm_and_jd.agency_roles << AgencyRole.find_by_role(AgencyRole::ROLE[:CM])
+agency_cm_and_jd.agency_roles << AgencyRole.find_by_role(AgencyRole::ROLE[:JD])
+agency_cm_and_jd.save!
+agency_cm_and_jd.agency_relations <<
+      AgencyRelation.new(agency_role: AgencyRole.find_by_role(AgencyRole::ROLE[:CM]),
+                          job_seeker: js1)
+agency_cm_and_jd.agency_relations <<
+      AgencyRelation.new(agency_role: AgencyRole.find_by_role(AgencyRole::ROLE[:JD]),
+                          job_seeker: js2)
+agency_cm_and_jd.save!
 
-agency_person = AgencyPerson.new(first_name: 'Jane', last_name: 'Doe', 
-                      agency_id: agency.id, email: 'jane@metplus.org', 
+agency_jd = AgencyPerson.new(first_name: 'Jane', last_name: 'Doe',
+                      agency_id: agency.id, email: 'jane@metplus.org',
                       password: 'qwerty123', confirmed_at: Time.now,
                       branch_id: agency.branches[2].id,
                       status: AgencyPerson::STATUS[:ACT])
-agency_person.agency_roles << AgencyRole.find_by_role(AgencyRole::ROLE[:JD])
-agency_person.save!
+agency_jd.agency_roles << AgencyRole.find_by_role(AgencyRole::ROLE[:JD])
+agency_jd.save!
+agency_jd.agency_relations <<
+      AgencyRelation.new(agency_role: AgencyRole.find_by_role(AgencyRole::ROLE[:JD]),
+                          job_seeker: js3)
 
 # Job Categories
-JobCategory.create(name: 'SW Developer - RoR', 
+JobCategory.create(name: 'SW Developer - RoR',
             description: 'Ruby on Rails backend developer')
-JobCategory.create(name: 'SW Developer - JS', 
+JobCategory.create(name: 'SW Developer - JS',
             description:  'Javascript frontend developer')
-JobCategory.create(name: 'SW Developer - Java', 
+JobCategory.create(name: 'SW Developer - Java',
             description: 'Java backend developer')
-JobCategory.create(name: 'SW Project Manager - Agile', 
+JobCategory.create(name: 'SW Project Manager - Agile',
             description: 'Manages Agile SW development projects')
-JobCategory.create(name: 'SW Project Manager - Waterfall', 
+JobCategory.create(name: 'SW Project Manager - Waterfall',
             description: 'Manages SW development projects using waterfall SDLC')
-JobCategory.create(name: 'Product Manager - SaaS', 
+JobCategory.create(name: 'Product Manager - SaaS',
             description: 'Manages SaaS product development and commecialization')
-            
-# Job Seekers
-JobSeeker.create(first_name: 'Tom', last_name: 'Seeker', 
-                      email: 'tom@gmail.com', password: 'qwerty123', 
-              year_of_birth: '1980', resume: 'text',
-          job_seeker_status: @jss1, confirmed_at: Time.now)
-                      
-JobSeeker.create(first_name: 'Mary', last_name: 'McCaffrey', 
-                      email: 'mary@gmail.com', password: 'qwerty123', 
-              year_of_birth: '1970', resume: 'text',
-          job_seeker_status: @jss2, confirmed_at: Time.now)
-                      
-JobSeeker.create(first_name: 'Frank', last_name: 'Williams', 
-                      email: 'frank@gmail.com', password: 'qwerty123', 
-              year_of_birth: '1970', resume: 'text',
-          job_seeker_status: @jss3, confirmed_at: Time.now)
