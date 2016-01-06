98045b0d30f97aee16da588fdd7cb6c0c474fb7f
vijayakarumudi <vijaya.karumudi@yahoo.com>
2016-01-06 13:51:33 -0400

    updates for action mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,7 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+   config.action_mailer.default_url_options = { :host => 'localhost' }
  
 end
