1076eac2f2e75734a181e4f0478bf2e088933ad9
Salem Amba <salemamba@gmail.com>
2016-01-15 20:06:06 -0800

    add initilize precompile to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.initialize_on_precompile = false
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
