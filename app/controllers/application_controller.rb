7b07c8753065ac24b8706faa13bc9de2ecfe9630
Salem Amba <salemamba@gmail.com>
2016-01-24 14:35:51 -0800

    structured applciation_controller and removed helper file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,12 +13,13 @@ class ApplicationController < ActionController::Base
     end
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :first_name
-      devise_parameter_sanitizer.for(:sign_up) << :last_name    
-      devise_parameter_sanitizer.for(:sign_up) << :phone
-        
+      
       [:first_name,:last_name, :phone].each{ |field|
         devise_parameter_sanitizer.for(:account_update)<<field  
+      }
+
+      [:first_name,:last_name, :phone].each{ |field|
+        devise_parameter_sanitizer.for(:sign_up)<<field  
       }
     end
 end
