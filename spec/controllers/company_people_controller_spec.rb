cd28f7e9a6cea832a5fb9254c245992661f6c886
Amy Lightholder <sidhene@gmail.com>
2016-01-25 13:35:21 -0800

    spec tests for companyperson

--- spec/controllers/company_people_controller_spec.rb
+++ spec/controllers/company_people_controller_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe CompanyPeopleController, type: :controller do
+  describe "GET #edit" do
+    before(:each) do
+      @companyperson = FactoryGirl.create(:company_person)
+      get :edit, id: @companyperson
+    end
 
+    it "renders edit template" do
+      expect(response).to render_template 'edit'
+    end
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PATCH #update" do
+
+    context "valid attributes" do
+      before(:each) do
+        @companyperson = FactoryGirl.create(:company_person)
+        patch :update, id: @companyperson, company_person: FactoryGirl.attributes_for(:company_person)
+
+      end
+
+      it 'sets flash message' do
+         expect(flash[:notice]).to eq "Your profile was updated successfully."
+      end
+      it 'returns redirect status' do
+         expect(response).to have_http_status(:redirect)
+      end
+      it 'redirects to mainpage' do
+         expect(response).to redirect_to(root_path)
+      end
+     end
+     context "valid attributes without password change" do
+       before(:each) do
+         @companyperson =  FactoryGirl.create(:company_person)
+         @user =  FactoryGirl.create(:user)
+         @companyperson.valid?
+         patch :update, company_person:FactoryGirl.attributes_for(:company_person, year_of_birth: '1980').merge(FactoryGirl.attributes_for(:user, first_name:'John',last_name:'Smith',phone:'780-890-8976')),id:@companyperson
+         @companyperson.reload
+         @user.reload
+
+       end
+      it 'sets a firstname' do
+         expect(@companyperson.first_name).to eq ("John")
+      end
+      it 'sets a lastname' do
+         expect(@companyperson.last_name).to eq ("Smith")
+      end
+      it 'sets flash message' do
+         expect(flash[:notice]).to eq "Your profile was updated successfully."
+      end
+      it 'returns redirect status' do
+         expect(response).to have_http_status(:redirect)
+      end
+      it 'redirects to mainpage' do
+        expect(response).to redirect_to(root_path)
+      end
+     end
+
+  end
 end
