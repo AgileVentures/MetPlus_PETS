b5707bfde51119f9df5b810cc195143bdc9732d9
Salem Amba <salemamba@gmail.com>
2016-01-24 16:43:25 -0800

    deleted unused steps in user_steps file

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -17,11 +17,6 @@ When(/^I visit profile for "(\w+)"$/) do |first_name|
    visit  "/users/edit.#{user.id}"
 end
 
-# Given(/^I am on edit page for "(\w+)"$/) do |name|
-#   step "I visit profile for #{name}"
-# end
-
-
 Then(/^I should verify the change of first_name "(.*?)", last_name "(.*?)" and phone "(.*?)"$/) do |first_name, last_name, phone|
     user = User.find_by_first_name(first_name)
     expect(user.last_name).to    eql last_name
