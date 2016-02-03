74816fa6430442cb39516c1f4f4f281ece226067
Amy Lightholder <sidhene@gmail.com>
2016-02-03 10:50:10 -0800

    PR change requests and fixing Puma

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Rails.application.routes.draw do
   # ----------------------- Company People -----------------------------------
   # Company admin (and agency admin) can edit and delete a company person
   resources :company_people, path: '/company_admin/company_people',
-                       only: [:create, :show, :edit, :update, :destroy]
+                       only: [:show, :edit, :update, :destroy]
 
   resources :company_people do
      get 'edit_profile', on: :member, as: :edit_profile
