6c727c0560524561a88322371c092b247a1ca4f2
Salem Amba <salemamba@gmail.com>
2016-02-03 17:41:04 -0800

    implemented title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "MetPlus"
+   
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end 
+  end
+  
   def flash_to_css key
     case key
       when 'notice'
