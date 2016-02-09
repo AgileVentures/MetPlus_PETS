e495694455dae74dfd331e19cfe89aea571a62ce
Patrick Bolger <patmbolger@gmail.com>
2016-02-09 07:02:01 -0500

    added pusher.js

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -2,11 +2,12 @@ var PusherTest = {
 
   setup: function () {
     var pusher = new Pusher('1d3d6764932fac0ac318');
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('new-message', function(data) {
+    var channel = pusher.subscribe('my_channel');
+    channel.bind('new_message', function(data) {
       alert('An event was triggered with message: ' + data.message);
     });
   }
 };
 
 $(PusherTest.setup);
+$(PusherTest) {alert('this is a test')};
