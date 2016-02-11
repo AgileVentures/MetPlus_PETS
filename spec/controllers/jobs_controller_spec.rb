1c923a2c471ae120b71ca4e94543f9c7e68c6a61
Salem Amba <salemamba@gmail.com>
2016-02-11 11:09:51 -0800

    updated the jobs spec

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 RSpec.describe JobsController, type: :controller do
   
   describe "Route => index" do
-    it { should route(:get, "/")
-      .to(action: :index) }
-    # it { should route(:get, "/jobs")
-              
+    it { should route(:get, "/jobs")
+      .to(action: :index) }            
   end
 
   describe "Route => new" do
