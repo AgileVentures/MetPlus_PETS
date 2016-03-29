31ecb42ad5cbc45f49a98674b1cbc34f1c87fc35
Patrick Bolger <patmbolger@gmail.com>
2016-03-29 10:29:44 -0400

    interim progress

---
+++ app/mailers/agency_mailer.rb
@@ -0,0 +1,9 @@
+class AgencyMailer < ApplicationMailer
+
+  def job_seeker_registered
+  end
+
+  def company_registered
+  end
+  
+end
---
+++ spec/mailers/previews/agency_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/agency_mailer
+class AgencyMailerPreview < ActionMailer::Preview
+
+end
