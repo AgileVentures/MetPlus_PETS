18723fedc0e9ad7d9b10f25431f71c913dc0759e
Patrick Bolger <patmbolger@gmail.com>
2016-03-29 14:24:52 -0400

    interim progress

--- app/mailers/agency_mailer.rb
+++ app/mailers/agency_mailer.rb
@@ -1,9 +1,22 @@
 class AgencyMailer < ApplicationMailer
 
-  def job_seeker_registered
+  def job_seeker_registered(agency_person, js_name, js_id)
+    send_notification_mail(agency_person, js_name, js_id, 'Job Seeker')
   end
 
-  def company_registered
+  def company_registered(agency_person, company_name, company_id)
+    send_notification_mail(agency_person, company_name, company_id, 'Company')
   end
-  
+
+  private
+
+  def send_notification_mail(agency_person, obj_name, obj_id, obj_type)
+    @agency_person = agency_person
+    @obj_name = obj_name
+    @obj_id   = obj_id
+    @obj_type = obj_type
+    mail to: agency_person.email,
+         template_name: 'agency_notification'
+  end
+
 end
