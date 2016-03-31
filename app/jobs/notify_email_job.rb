f43e472cc309008108bd09f256c8df3c01644cd3
Patrick Bolger <patmbolger@gmail.com>
2016-03-31 12:28:18 -0400

    interim progress

---
+++ app/jobs/notify_email_job.rb
@@ -0,0 +1,21 @@
+class NotifyEmailJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(email_addresses, evt_type, evt_data)
+
+    case evt_type
+      
+    when Event::EVT_TYPE[:JS_REGISTER]
+
+      AgencyMailer.job_seeker_registered(email_addresses,
+                    evt_data[:name],
+                    evt_data[:id]).deliver_later
+
+    when Event::EVT_TYPE[:COMP_REGISTER]
+
+      AgencyMailer.company_registered(email_addresses,
+                    evt_data[:name],
+                    evt_data[:id]).deliver_later
+    end
+  end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -11,19 +11,17 @@ class Event
       Pusher.trigger('pusher_control',
                      EVT_TYPE[:JS_REGISTER], evt_data)
 
-      # Send notification email to all agency personnel
-      agency_emails = Agency.all_agency_people_emails
-      AgencyMailer.job_seeker_registered(agency_emails, evt_data[:name],
-                    evt_data[:id]).deliver_later
+      NotifyEmailJob.set(wait: 10.seconds).
+                     perform_later(Agency.all_agency_people_emails,
+                     EVT_TYPE[:JS_REGISTER], evt_data)
 
     when :COMP_REGISTER   # company registration request
       Pusher.trigger('pusher_control',
                      EVT_TYPE[:COMP_REGISTER], evt_data)
 
-      # Send notification email to all agency personnel
-      agency_emails = Agency.all_agency_people_emails
-      AgencyMailer.company_registered(agency_emails, evt_data[:name],
-                    evt_data[:id]).deliver_later
+      NotifyEmailJob.set(wait: 10.seconds).
+                     perform_later(Agency.all_agency_people_emails,
+                     EVT_TYPE[:COMP_REGISTER], evt_data)
     end
   end
 
