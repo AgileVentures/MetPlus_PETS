594fdf4a368fe8cc4fe0281d66c0af64aeabff28
Joao Pereira <joaopapereira@gmail.com>
2016-04-01 18:26:13 -0400

    Added business logic tests and information

--- lib/task_manager/task_manager.rb
+++ lib/task_manager/task_manager.rb
@@ -1,7 +1,10 @@
 module TaskManager
+
   STATUS = ['New', 'Work in progress', 'Done', 'Assigned']
 
+  ## The methods from this module will be available at Class level
   module ClassMethods
+    ## Method that will create a task, give an audience(In the format needed by the task), the type and more arguments
     def create_task(audience, task_type, *args)
       person = nil
       job = nil
@@ -12,7 +15,10 @@ module TaskManager
         company = arg if arg.is_a? Company
       end
       task = send("new_#{task_type}".to_sym, audience, person, job, company)
-      schedule_event :taskCreated, task, :AA
+
+      if not args.include? 'no_events'
+        schedule_event :taskCreated, task, :AA
+      end
       return task
     end
 
@@ -29,14 +35,16 @@ module TaskManager
       method_name =~ /^new_(.+)$/ || super
     end
 
-
+    ## Method used to schedule a future event, this functionality is not yet defined
     def schedule_event event_type, task, role_to_notify
     end
 
+    ## Method used to unschedule one event, this functionality is not yet defined
     def unschedule_event event_type, task, role_to_notify
     end
 
     private
+    ## Method that will create the task itself
     def new_task task_type, owner, target_user = nil, target_job = nil, target_company = nil
       task = Task.new
       task.task_owner = owner
@@ -50,6 +58,8 @@ module TaskManager
     end
 
   end
+
+  ## The functions inside this module will not be used they are here just for information
   module InstanceMethods
     private
     def wip_test task
@@ -60,16 +70,19 @@ module TaskManager
     end
   end
 
+  ## Assign the current task to a specific person
   def assign person
     raise ArgumentError, 'Task need to be in created state' if status != STATUS[0]
     send("assign_#{task_type}".to_sym, self, person)
   end
 
+  ## Change the status of the task to Work In Progress
   def work_in_progress
     raise ArgumentError, 'Task need to be in assigned state' if status != STATUS[3]
     send("wip_#{task_type}".to_sym, self)
   end
 
+  ## Change the status of the task to Complete
   def complete
     raise ArgumentError, 'Task need to be in work in progress state' if status != STATUS[1]
     send("done_#{task_type}".to_sym, self)
@@ -77,11 +90,11 @@ module TaskManager
 
   def method_missing(meth, *args, &block)
     if meth.to_s =~ /^wip_(.+)$/
-      return TaskManager::wip_default self
+      return TaskManager::wip_default self, *args, &block
     elsif meth.to_s =~ /^done_(.+)$/
-      return TaskManager::done_default self
+      return TaskManager::done_default self, *args, &block
     elsif meth.to_s =~ /^assign_(.+)$/
-      return TaskManager::assign_default self, *args
+      return TaskManager::assign_default self, *args, &block
     else
       super meth, *args, &block
     end
@@ -92,31 +105,41 @@ module TaskManager
     method_name =~ /^(wip|done)_(.+)$/ || super
   end
 
-
   private
-
-  def self.assign_default task, person, *args
+  # Function that will assign a task to a person
+  # Default function called, if no other specific function exists
+  def self.assign_default task, person, *args, &block
     task.status = STATUS[3]
     task.task_owner = {:user => person}
     task.save
-    Task.unschedule_event :taskCreated, task, :AA
-    Task.schedule_event :taskAssigned, task, :AA
+    if args.size == 1 or args[1] != 'no_events'
+      Task.unschedule_event :taskCreated, task, :AA
+      Task.schedule_event :taskAssigned, task, :AA
+    end
     return task
   end
 
-  def self.wip_default task
+  # Function that will mark the task as Work in Progress
+  # Default function called, if no other specific function exists
+  def self.wip_default task, *args, &block
     task.status = STATUS[1]
     task.save
-    Task.unschedule_event :taskAssigned, task, :AA
-    Task.schedule_event :taskWorkStarted, task, :AA
+    if args.size == 1 or args[1] != 'no_events'
+      Task.unschedule_event :taskAssigned, task, :AA
+      Task.schedule_event :taskWorkStarted, task, :AA
+    end
     return task
   end
 
-  def self.done_default task
+  # Function that will mark the task as Done
+  # Default function called, if no other specific function exists
+  def self.done_default task, *args, &block
     task.status = STATUS[2]
     task.save
-    Task.unschedule_event :taskWorkStarted, task, :AA
-    Task.schedule_event :taskDone, task, :AA
+    if args.size == 1 or args[1] != 'no_events'
+      Task.unschedule_event :taskWorkStarted, task, :AA
+      Task.schedule_event :taskDone, task, :AA
+    end
     return task
   end
 
