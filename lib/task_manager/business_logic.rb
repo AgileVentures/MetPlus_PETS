919c6e2474503f07b06504498b6747350faef9ba
Joao Pereira <joaopapereira@gmail.com>
2016-04-01 18:26:13 -0400

    Added business logic tests and information

--- lib/task_manager/business_logic.rb
+++ lib/task_manager/business_logic.rb
@@ -1,4 +1,40 @@
 module BusinessLogic
+##
+## Module that contain all the business logic associated with tasks
+##
+## For each new task we need to:
+## 1 - Add a description of the task to DESCRIPTIONS hash
+## 2 - Create a function inside ClassMethods module called new_[name]_task
+##      where name is the descriptive name of the new task
+##
+##
+## If we need to overload the assign method for the event "my_event"
+## 1 - Add a function in InstanceMethods called that looks like this:
+##     def assign_my_event person
+##         assign_default person, 'no_events'
+##         # your code here
+##     end
+##   The 'no_events' tell the function to do not set events for this specific task or if you want to set the events
+##   using your code
+##
+## If we need to overload the work_in_progress method for the event "my_event"
+## 1 - Add a function in InstanceMethods called that looks like this:
+##     def wip_my_event person
+##         wip_default 'no_events'
+##         # your code here
+##     end
+##   The 'no_events' tell the function to do not set events for this specific task or if you want to set the events
+##   using your code
+##
+## If we need to overload the complete method for the event "my_event"
+## 1 - Add a function in InstanceMethods called that looks like this:
+##     def done_my_event person
+##         done_default 'no_events'
+##         # your code here
+##     end
+##   The 'no_events' tell the function to do not set events for this specific task or if you want to set the events
+##   using your code
+##
 
   DESCRIPTIONS = {:need_job_developer => 'Job Seeker have no assigned Job Developer',
                   :need_case_manager  => 'Job Seeker have no assigned Case Manager'}
@@ -9,10 +45,10 @@ module BusinessLogic
        new_js_unassigned_cm_task(jobseeker, agency)]
     end
     def new_js_unassigned_jd_task jobseeker, agency
-      create_task({:agency => {agency: agency, role: :JD}}, :need_job_developer, jobseeker)
+      create_task({:agency => {agency: agency, role: :AA}}, :need_job_developer, jobseeker)
     end
     def new_js_unassigned_cm_task jobseeker, agency
-      create_task({:agency => {agency: agency, role: :CM}}, :need_case_manager, jobseeker)
+      create_task({:agency => {agency: agency, role: :AA}}, :need_case_manager, jobseeker)
     end
   end
   module InstanceMethods
