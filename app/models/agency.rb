a83780d81ec8d94cc09d7e9078c3445bd4135f2e
Amy Lightholder <sidhene@gmail.com>
2016-04-06 10:57:32 -0700

    change requests

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -42,13 +42,6 @@ class Agency < ActiveRecord::Base
 
   def self.find_users_with_role(agency, role)
     agency.agency_people_on_role role
-    users = []
-    agency.agency_people.each do |ap|
-                users << ap if ap.agency_roles &&
-                               ap.agency_roles.pluck(:role).include?(role)
-    end
-
-    users
   end
 
 
