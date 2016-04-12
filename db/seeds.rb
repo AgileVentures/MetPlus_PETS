7cee8f15b8eb6b58ae40817f9b6b559762124123
Joao Pereira <joaopapereira@gmail.com>
2016-04-12 11:05:18 -0400

    Add staging environment and seeds file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,64 +1,21 @@
 
 require 'ffaker'
 
-# case Rails.env
-#   when "development"
-#     comp1 = FactoryGirl.create(:company, :name => 'Company 1', :email => 'info@company1.com')
-#     comp2 = FactoryGirl.create(:company, :name => 'Company 2', :email => 'info@company2.com')
-#     emp1 = FactoryGirl.create(:employer, :first_name => 'John', :email => 'john@company1.com', :company => comp1)
-#     emp2 = FactoryGirl.create(:employer, :first_name => 'Tom', :email => 'tom@company1.com', :company => comp1)
-#     emp3 = FactoryGirl.create(:employer, :first_name => 'Mary', :email => 'mary@company2.com', :company => comp2)
-
-#     skill1 = FactoryGirl.create(:skill, :name => 'Handyman', :description => 'Some handy man')
-#     skill2 = FactoryGirl.create(:skill, :name => 'Software Developer', :description => 'Develops software')
-#     skill3 = FactoryGirl.create(:skill, :name => 'Cook', :description => 'Cooks very nice food')
-#     skill4 = FactoryGirl.create(:skill, :name => 'English teacher', :description => 'Teaches english')
-#     skill5 = FactoryGirl.create(:skill, :name => 'Manager', :description => 'Someone that manages')
-#     skill6 = FactoryGirl.create(:skill, :name => 'Administrative', :description => 'Administrative work')
-
-#     job1 = FactoryGirl.create(:job, :company => comp1, :employer => emp1, :title => 'Software developer',
-#                               :description => 'Looking for a software developer that can work miracles')
-#     job1.required_skills << skill2
-#     job1.nice_to_have_skills << skill5
-#     job1.save
-
-#     job2 = FactoryGirl.create(:job, :company => comp1, :employer => emp2, :title => 'Teacher',
-#                               :description => 'Looking for a nice english teacher to do some teaching')
-#     job2.required_skills << skill4
-#     job2.save
-
-#     job3 = FactoryGirl.create(:job, :company => comp2, :employer => emp3, :title => 'The Cook',
-#                               :description => 'Cooking that can cook and do some handywork')
-#     job3.required_skills << skill3
-#     job3.nice_to_have_skills << skill1
-#     job3.save
-
-#     job4 = FactoryGirl.create(:job, :company => comp1, :employer => emp3, :title => 'Need a person',
-#                               :description => 'Need someone that can do a bunch of things')
-#     job4.required_skills << skill6
-#     job4.required_skills << skill5
-#     job4.nice_to_have_skills << skill1
-#     job4.nice_to_have_skills << skill4
-#     job4.save
-# end
-# User.find_or_create_by(email: 'salemamba1@gmail.com') do |user|
-#     user.first_name = "salem"
-#     user.last_name  = 'amba'
-#     user.password = 'secret123'
-#     user.password_confirmation = 'secret123'
-#     user.phone ='619-316-8971'
-#     user.confirmed_at = DateTime.now
-# end
-
 puts "Seeding wait......."
 #incase rerun comes in. not necessary
+AgencyRelation.delete_all
+JobSeeker.delete_all
 JobSeekerStatus.delete_all
 Job.delete_all
-Company.delete_all
-Address.delete_all
 CompanyPerson.delete_all
+Address.delete_all
+Company.delete_all
 JobCategory.delete_all
 Skill.delete_all
+AgencyPerson.delete_all
+Branch.delete_all
+Agency.delete_all
+User.delete_all
 
 # --------------------------- Seed Production Database --------------------
 
@@ -101,7 +58,7 @@ agency = Agency.create!(name: 'MetPlus', website: 'metplus.org',
 puts "\nSeeded Production Data"
 
 # seed striction to development, for now
-if Rails.env.development? # || Rails.env.staging?
+if Rails.env.development? || Rails.env.staging?
 
   #-------------------------- Companies -----------------------------------
   200.times do |n|
@@ -301,15 +258,15 @@ if Rails.env.development? # || Rails.env.staging?
 
   branch = Branch.create(code: '001', agency: agency)
   branch.address = Address.create!(city: 'Detroit',
-              street: '123 Main Street', zipcode: 48201)
+              street: '123 Main Street', state: 'MI', zipcode: 48201)
 
   branch = Branch.create(code: '002', agency: agency)
   branch.address = Address.create!(city: 'Detroit',
-              street: '456 Sullivan Street', zipcode: 48204)
+              street: '456 Sullivan Street', state: 'MI', zipcode: 48204)
 
   branch = Branch.create(code: '003', agency: agency)
   branch.address = Address.create!(city: 'Detroit',
-              street: '3 Auto Drive', zipcode: 48206)
+              street: '3 Auto Drive', state: 'MI', zipcode: 48206)
 
   puts "Branches created: #{Branch.count}"
 
