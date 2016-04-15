345adbd50dc6e8a93f771573928358d6ef114e2d
Joao Pereira <joaopapereira@gmail.com>
2016-04-15 12:44:30 -0400

    Added implementation

--- app/models/job_seeker.rb
+++ app/models/job_seeker.rb
@@ -7,6 +7,8 @@ class JobSeeker < ActiveRecord::Base
   has_many   :agency_relations
   has_many   :agency_people, through: :agency_relations
 
+  has_many    :job_applications
+
   validates_presence_of :year_of_birth, :job_seeker_status_id #,:resume
   validates  :year_of_birth, :year_of_birth => true
 
