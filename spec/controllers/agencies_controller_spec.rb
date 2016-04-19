87ec551a2e6cb4b7aaddd3802cb616051df0bd01
Joao Pereira <joaopapereira@gmail.com>
2016-04-19 09:55:16 -0400

    Added missing test and corrected test localization

--- spec/controllers/agencies_controller_spec.rb
+++ spec/controllers/agencies_controller_spec.rb
@@ -3,36 +3,39 @@ require 'rails_helper'
 RSpec.describe AgenciesController, type: :controller do
 
   describe "GET #edit" do
-    before(:each) do
-      @agency = FactoryGirl.create(:agency)
-      agency_admin = FactoryGirl.create(:agency_admin, agency: @agency)
-      sign_in agency_admin
-      get :edit, id: @agency
+    context 'success' do
+      before(:each) do
+        @agency = FactoryGirl.create(:agency)
+        agency_admin = FactoryGirl.create(:agency_admin, agency: @agency)
+        sign_in agency_admin
+        get :edit, id: @agency
+      end
+      it 'assigns @agency for form' do
+        expect(assigns(:agency)).to eq @agency
+      end
+      it 'renders edit template' do
+        expect(response).to render_template('edit')
+      end
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
     end
-    it 'assigns @agency for form' do
-      expect(assigns(:agency)).to eq @agency
-    end
-    it 'renders edit template' do
-      expect(response).to render_template('edit')
-    end
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
+    context "as Job Developer" do
+      before(:each) do
+        @agency = FactoryGirl.create(:agency)
+        job_developer = FactoryGirl.create(:job_developer, agency: @agency)
+        sign_in job_developer
+        get :edit, id: @agency
+      end
+      it "redirect" do
+        expect(response).to have_http_status 302
+      end
+      it 'sets flash message' do
+        expect(flash[:alert]).to eq "You are not authorized to edit #{@agency.name} agency."
+      end
     end
   end
-  describe "GET #edit as Job Developer" do
-    before(:each) do
-      @agency = FactoryGirl.create(:agency)
-      agency_admin = FactoryGirl.create(:job_developer, agency: @agency)
-      sign_in agency_admin
-      get :edit, id: @agency
-    end
-    it "redirect" do
-      expect(response).to have_http_status 302
-    end
-    it 'sets flash message' do
-      expect(flash[:warning]).to eq "You are not authorized to access this page."
-    end
-  end
+
   describe "PATCH #update" do
     context 'valid attributes' do
       before(:each) do
@@ -73,6 +76,21 @@ RSpec.describe AgenciesController, type: :controller do
       end
       it "returns http success" do
         expect(response).to have_http_status(:success)
+      end
+    end
+    describe "Try to edit as Job Developer" do
+      before(:each) do
+        @agency = FactoryGirl.create(:agency)
+        job_developer = FactoryGirl.create(:job_developer, agency: @agency)
+        sign_in job_developer
+        patch :update, agency: FactoryGirl.attributes_for(:agency,
+                                                          phone: '', website: 'nodomain'), id: @agency
+      end
+      it "redirect" do
+        expect(response).to have_http_status 302
+      end
+      it 'sets flash message' do
+        expect(flash[:alert]).to eq "You are not authorized to edit #{@agency.name} agency."
       end
     end
   end
