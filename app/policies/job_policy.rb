244242b18f26f42ec65cde87b7d96bcfdd70413a
Joao Pereira <joaopapereira@gmail.com>
2016-04-27 12:02:45 -0400

    Added feature tests

--- app/policies/job_policy.rb
+++ app/policies/job_policy.rb
@@ -1,8 +1,9 @@
 class JobPolicy < ApplicationPolicy
   def apply?
-    user.is_job_seeker? ||
-        (user.is_a?(AgencyPerson) &&
-            (user.is_job_developer?(user.agency) ||
-            user.is_case_manager?(user.agency)))
+    not user.nil? and
+      (user.is_job_seeker? or
+          (user.is_a?(AgencyPerson) and
+              (user.is_job_developer?(user.agency) or
+              user.is_case_manager?(user.agency))))
   end
 end
