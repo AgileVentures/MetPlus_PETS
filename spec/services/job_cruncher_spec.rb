4b43297e83be90fd26ac3b80b24475a66f0135c7
vijayakarumudi <vijaya.karumudi@yahoo.com>
2016-05-10 17:35:08 -0300

    Updates for job cruncher

--- spec/services/job_cruncher_spec.rb
+++ spec/services/job_cruncher_spec.rb
@@ -4,7 +4,7 @@ include ActionDispatch::TestProcess
 RSpec.describe JobCruncher, type: :model do
 
   before(:each) do
-   stub_request(:post,CruncherService.service_url + '/authencate').
+   stub_request(:post,CruncherService.service_url + '/authenticate').
        to_return(body: "{\"token\": \"12345\"}", status: 200,
         :headers => {'Content-Type' => 'application/json'})
   end
@@ -13,7 +13,7 @@ RSpec.describe JobCruncher, type: :model do
     it 'returns success (true) for create a new job' do
 
        stub_request(:post, CruncherService.service_url + '/job/create').
-           to_return(body: "{\"resultcode\":\"SUCCESS\"}" , status: 200,
+           to_return(body: "{\"resultCode\":\"SUCCESS\"}" , status: 200,
            :headers => {'Content-Type' => 'application/json'})
        expect(JobCruncher.create_job(10,'Software Engineer',
               'description of the job')).to be true
@@ -22,7 +22,7 @@ RSpec.describe JobCruncher, type: :model do
     it 'returns fail(false) for create a new job' do
 
        stub_request(:post, CruncherService.service_url + '/job/create').
-           to_return(body: "{\"resultcode\":\"Failure\"}" ,
+           to_return(body: "{\"resultCode\":\"Failure\"}" ,
            :headers => {'Content-Type' => 'application/json'})
        expect(JobCruncher.create_job(11,'Software Engineer2',
               'description of the job2')).to be false
@@ -31,7 +31,7 @@ RSpec.describe JobCruncher, type: :model do
     it 'returns success (true) for job already exists' do
 
        stub_request(:post, CruncherService.service_url + '/job/create').
-           to_return(body: "{\"resultcode\":\"SUCCESS\"}" , status: 200,
+           to_return(body: "{\"resultCode\":\"SUCCESS\"}" , status: 200,
            :headers => {'Content-Type' => 'application/json'})
        expect(JobCruncher.create_job(10,'Software Engineer',
               'description of the job')).to be true
