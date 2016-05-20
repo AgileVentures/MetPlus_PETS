cc01a2f73e06dc01fce27201dfb74ea8479f5126
vijayakarumudi <vijaya.karumudi@yahoo.com>
2016-05-20 14:09:45 -0300

    modified job cruncher service spec

--- spec/services/job_cruncher_spec.rb
+++ spec/services/job_cruncher_spec.rb
@@ -19,22 +19,22 @@ RSpec.describe JobCruncher, type: :model do
               'description of the job')).to be true
     end
 
-    it 'returns fail(false) for create a new job' do
+    it 'returns fail(false) if cruncher create fails' do
 
        stub_request(:post, CruncherService.service_url + '/job/create').
-           to_return(body: "{\"resultCode\":\"Failure\"}" ,
+           to_return(body: "{\"resultCode\":\"FAILURE\"}" ,
            :headers => {'Content-Type' => 'application/json'})
        expect(JobCruncher.create_job(11,'Software Engineer2',
               'description of the job2')).to be false
     end
 
-    it 'returns success (true) for job already exists' do
+    it 'returns failure (false) for job already exists' do
 
        stub_request(:post, CruncherService.service_url + '/job/create').
-           to_return(body: "{\"resultCode\":\"SUCCESS\"}" , status: 200,
+           to_return(body: "{\"resultCode\":\"JOB_ID_EXISTS\"}" , status: 200,
            :headers => {'Content-Type' => 'application/json'})
        expect(JobCruncher.create_job(10,'Software Engineer',
-              'description of the job')).to be true
+              'description of the job')).to be false
     end
   end
  end
