22f235d2a194aaa68dbaca39113d4c9a6ca85a92
Patrick Bolger <patmbolger@gmail.com>
2016-05-26 15:16:23 -0400

    updated jobs#show view

--- app/models/job_skill.rb
+++ app/models/job_skill.rb
@@ -4,6 +4,8 @@ class JobSkill < ActiveRecord::Base
   belongs_to :skill_level
 
   validates_presence_of :job, :skill
+  validates_uniqueness_of :skill_id, scope: :job_id
+
   validates :required, inclusion: { in: [true, false] }
   validates_numericality_of :min_years, only_integer: true,
                       greater_than_or_equal_to: 0,
