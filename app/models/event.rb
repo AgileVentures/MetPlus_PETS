a57f346ef88baf92af0053fbeeef9af8c6fc3c4a
Patrick Bolger <patmbolger@gmail.com>
2016-06-13 18:58:59 -0400

    refactored Event.create

--- app/models/event.rb
+++ app/models/event.rb
@@ -26,109 +26,133 @@ class Event
 
   def self.create(evt_type, evt_obj)
     case evt_type
-    when :JS_REGISTER                       # evt_obj = job seeker
-      Pusher.trigger('pusher_control',
-                     EVT_TYPE[:JS_REGISTER],
-                     {id: evt_obj.id,
-                      name: evt_obj.full_name(last_name_first: false)})
-
-      NotifyEmailJob.set(wait: @@delay.seconds).
-                     perform_later(Agency.all_agency_people_emails,
-                     EVT_TYPE[:JS_REGISTER],
-                     evt_obj)
-
-      # MULTIPLE AGENCIES: the code below needs to change
-      Task.new_js_registration_task(evt_obj, Agency.first)
-
-    when :COMP_REGISTER                     # evt_obj = company
-      # Business rules:
-      #    Notify agency people (pop-up and email)
-      #    Send email to company contact
-      #    Add task to review company registration
-      Pusher.trigger('pusher_control',
-                     EVT_TYPE[:COMP_REGISTER],
-                     {id: evt_obj.id, name: evt_obj.name})
-
-      NotifyEmailJob.set(wait: @@delay.seconds).
-                     perform_later(Agency.all_agency_people_emails,
-                     EVT_TYPE[:COMP_REGISTER],
-                     evt_obj)
-
-      CompanyMailer.pending_approval(evt_obj,
-                                     evt_obj.company_people[0]).deliver_now
-
-      Task.new_review_company_registration_task(evt_obj, evt_obj.agencies[0])
-
-    when :COMP_APPROVED                     # evt_obj = company
-      # Business rules:
-      #    Send email to company contact
-
-      CompanyMailer.registration_approved(evt_obj,
-                                          evt_obj.company_people[0]).deliver_now
-
-    when :COMP_DENIED                     # evt_obj = struct(company, reason)
-      # Business rules:
-      #    Send email to company contact
-
-      CompanyMailer.registration_denied(evt_obj.company,
-                                        evt_obj.company.company_people[0],
-                                        evt_obj.reason).deliver_now
-
-    when :JS_APPLY                          # evt_obj = job application
-      # Business rules:
-      #    Notify job seeker's case manager
-      #    Notify job seeker's job developer
-      #    Notify company contact associated with the job
-      #    Create task for 'job application' (application to be reviewed)
-
-      notify_list = notify_list_for_js_apply_event(evt_obj)
-
-      unless notify_list.empty?
-        Pusher.trigger('pusher_control',
-                       EVT_TYPE[:JS_APPLY],
-                       {job_id:  evt_obj.job.id,
-                        js_id:   evt_obj.job_seeker.id,
-                        js_name: evt_obj.job_seeker.full_name(last_name_first: false),
-                        notify_list: notify_list[0]})
-
-        NotifyEmailJob.set(wait: @@delay.seconds).
-                       perform_later(notify_list[1],
-                       EVT_TYPE[:JS_APPLY],
-                       evt_obj)
-      end
-
-      Task.new_review_job_application_task(evt_obj.job, evt_obj.job.company)
-
-    when :JS_ASSIGN_JD       # evt_obj = struct(:job_seeker, :agency_person)
-      # Business rules:
-      #    Notify job developer (email and popup)
-
-      Pusher.trigger('pusher_control',
-                     EVT_TYPE[:JS_ASSIGN_JD],
-                     {js_id:   evt_obj.job_seeker.id,
-                      js_name: evt_obj.job_seeker.full_name(last_name_first: false),
-                      jd_user_id: evt_obj.agency_person.user.id})
-
-      NotifyEmailJob.set(wait: @@delay.seconds).
-                     perform_later(evt_obj.agency_person.email,
-                     EVT_TYPE[:JS_ASSIGN_JD],
-                     evt_obj.job_seeker)
-
-    when :JS_ASSIGN_CM       # evt_obj = struct(:job_seeker, :agency_person)
-      # Business rules:
-      #    Notify case manager (email and popup)
-
-      Pusher.trigger('pusher_control',
-                     EVT_TYPE[:JS_ASSIGN_CM],
-                     {js_id:   evt_obj.job_seeker.id,
-                      js_name: evt_obj.job_seeker.full_name(last_name_first: false),
-                      cm_user_id: evt_obj.agency_person.user.id})
-
-      NotifyEmailJob.set(wait: @@delay.seconds).
-                     perform_later(evt_obj.agency_person.email,
-                     EVT_TYPE[:JS_ASSIGN_CM],
-                     evt_obj.job_seeker)
+    when :JS_REGISTER
+      evt_js_register(evt_type, evt_obj)
+    when :COMP_REGISTER
+      evt_comp_register(evt_type, evt_obj)
+    when :COMP_APPROVED
+      evt_comp_approved(evt_type, evt_obj)
+    when :COMP_DENIED
+      evt_comp_denied(evt_type, evt_obj)
+    when :JS_APPLY
+      evt_js_apply(evt_type, evt_obj)
+    when :JS_ASSIGN_JD
+      evt_js_assign_jd(evt_type, evt_obj)
+    when :JS_ASSIGN_CM
+      evt_js_assign_cm(evt_type, evt_obj)
     end
+  end
+
+  def self.evt_js_register(evt_type, evt_obj)  # evt_obj = job seeker
+    Pusher.trigger('pusher_control',
+                   EVT_TYPE[:JS_REGISTER],
+                   {id: evt_obj.id,
+                    name: evt_obj.full_name(last_name_first: false)})
+
+    NotifyEmailJob.set(wait: @@delay.seconds).
+                   perform_later(Agency.all_agency_people_emails,
+                   EVT_TYPE[:JS_REGISTER],
+                   evt_obj)
+
+    # MULTIPLE AGENCIES: the code below needs to change
+    Task.new_js_registration_task(evt_obj, Agency.first)
+  end
+
+  def self.evt_comp_register(evt_type, evt_obj)   # evt_obj = company
+    # Business rules:
+    #    Notify agency people (pop-up and email)
+    #    Send email to company contact
+    #    Add task to review company registration
+    Pusher.trigger('pusher_control',
+                   EVT_TYPE[:COMP_REGISTER],
+                   {id: evt_obj.id, name: evt_obj.name})
+
+    NotifyEmailJob.set(wait: @@delay.seconds).
+                   perform_later(Agency.all_agency_people_emails,
+                   EVT_TYPE[:COMP_REGISTER],
+                   evt_obj)
+
+    CompanyMailer.pending_approval(evt_obj,
+                                   evt_obj.company_people[0]).deliver_now
+
+    Task.new_review_company_registration_task(evt_obj, evt_obj.agencies[0])
+  end
+
+  def self.evt_comp_approved(evt_type, evt_obj)  # evt_obj = company
+    # Business rules:
+    #    Send email to company contact
+
+    CompanyMailer.registration_approved(evt_obj,
+                                        evt_obj.company_people[0]).deliver_now
+  end
+
+  def self.evt_comp_denied(evt_type, evt_obj) # evt_obj = struct(company, reason)
+    # Business rules:
+    #    Send email to company contact
+
+    CompanyMailer.registration_denied(evt_obj.company,
+                                      evt_obj.company.company_people[0],
+                                      evt_obj.reason).deliver_now
+  end
+
+  def self.evt_js_apply(evt_type, evt_obj)  # evt_obj = job application
+    # Business rules:
+    #    Notify job seeker's case manager
+    #    Notify job seeker's job developer
+    #    Notify company contact associated with the job
+    #    Create task for 'job application' (application to be reviewed)
+
+    notify_list = notify_list_for_js_apply_event(evt_obj)
+
+    unless notify_list.empty?
+      Pusher.trigger('pusher_control',
+                     EVT_TYPE[:JS_APPLY],
+                     {job_id:  evt_obj.job.id,
+                      js_id:   evt_obj.job_seeker.id,
+                      js_name: evt_obj.job_seeker.full_name(last_name_first: false),
+                      notify_list: notify_list[0]})
+
+      NotifyEmailJob.set(wait: @@delay.seconds).
+                     perform_later(notify_list[1],
+                     EVT_TYPE[:JS_APPLY],
+                     evt_obj)
+    end
+
+    Task.new_review_job_application_task(evt_obj.job, evt_obj.job.company)
+  end
+
+  def self.evt_js_assign_jd(evt_type, evt_obj)
+    # evt_obj = struct(:job_seeker, :agency_person)
+    # Business rules:
+    #    Notify job developer (email and popup)
+
+    Pusher.trigger('pusher_control',
+                   EVT_TYPE[:JS_ASSIGN_JD],
+                   {js_id:   evt_obj.job_seeker.id,
+                    js_name: evt_obj.job_seeker.full_name(last_name_first: false),
+                    jd_user_id: evt_obj.agency_person.user.id})
+
+    NotifyEmailJob.set(wait: @@delay.seconds).
+                   perform_later(evt_obj.agency_person.email,
+                   EVT_TYPE[:JS_ASSIGN_JD],
+                   evt_obj.job_seeker)
+  end
+
+  def self.evt_js_assign_cm(evt_type, evt_obj)
+    # evt_obj = struct(:job_seeker, :agency_person)
+    # Business rules:
+    #    Notify case manager (email and popup)
+
+    Pusher.trigger('pusher_control',
+                   EVT_TYPE[:JS_ASSIGN_CM],
+                   {js_id:   evt_obj.job_seeker.id,
+                    js_name: evt_obj.job_seeker.full_name(last_name_first: false),
+                    cm_user_id: evt_obj.agency_person.user.id})
+
+    NotifyEmailJob.set(wait: @@delay.seconds).
+                   perform_later(evt_obj.agency_person.email,
+                   EVT_TYPE[:JS_ASSIGN_CM],
+                   evt_obj.job_seeker)
   end
 
   def self.notify_list_for_js_apply_event(appl)
