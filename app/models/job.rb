c213a925eb703a20e4317d3824eda1a8bfac2b26
jeanwei <jeanwei938@msn.com>
2016-07-20 14:19:59 +0800

    remove skill_level from its dependents

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,7 +14,6 @@ class Job < ActiveRecord::Base
                 through: :job_skills, class_name: 'Skill', source: :skill
   has_many   :nice_to_have_skills, -> {where job_skills: {required: false}},
                 through: :job_skills, class_name: 'Skill', source: :skill
-  has_many   :skill_levels, through: :job_skills
   has_many   :job_applications
   has_many   :job_seekers, through: :job_applications
 
