649e58472ff99e73bf9d40ea01166628dc83bda0
tong wei <jeanwei938@msn.com>
2016-07-23 05:29:03 +0800

    Closes AgileVentures/MetPlus_tracker#328 (#332)
    * remove skill level from db
    
    * remove skill_level from its dependents
    
    * remove skill_level model,factory,spec

--- app/models/skill_level.rb
+++
@@ -1,6 +0,0 @@
-class SkillLevel < ActiveRecord::Base
-  validates_presence_of :name
-  validates_length_of :name, maximum: 20
-  validates_presence_of :description
-  validates_length_of :name, maximum: 100
-end
