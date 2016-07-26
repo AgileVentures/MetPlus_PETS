79d7aa5885d01f751cbeb7eb1237c94c18c835c2
DS Megha <dsmegha@gmail.com>
2016-07-26 14:18:34 +0530

    Fix #342

--- app/controllers/concerns/tasks.rb
+++ app/controllers/concerns/tasks.rb
@@ -6,7 +6,15 @@ module Tasks
       when 'mine-open'
         return Task.paginate(:page => params[:tasks_page], :per_page => per_page).find_by_owner_user_open pets_user
       when 'mine-closed'
-        return Task.paginate(:page => params[:tasks_page], :per_page => per_page).find_by_owner_user_closed pets_user
+        return Task.paginate(:page => params[:tasks_page], :per_page => per_page).find_by_owner_user_closed 
+       when 'agency-open'
+        return Task.paginate(:page => params[:tasks_page], :per_page => per_page).find_by_agency_open pets_user
+      when 'agency-closed'
+        return Task.paginate(:page => params[:tasks_page], :per_page => per_page).find_by_agency_closed pets_user
+      when 'company-open'
+        return Task.paginate(:page => params[:tasks_page], :per_page => per_page).find_by_company_open pets_user.agency
+      when 'company-closed'
+        return Task.paginate(:page => params[:tasks_page], :per_page => per_page).find_by_company_closed pets_user.agency  
     end
   end
 end
