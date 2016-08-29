d2d342064d090764c3166ebcc53b1c3ef1562ab6
Patrick Bolger <patmbolger@gmail.com>
2016-08-29 09:00:49 -0400

    reject works but pagination breaks

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -4,7 +4,7 @@ class JobApplicationsController < ApplicationController
 	before_action :find_application
 
 	def accept
-		unless @job_application.active? 
+		unless @job_application.active?
 			flash[:alert] = "Invalid action on inactive job application."
 		else
 			@job_application.accept
@@ -14,17 +14,29 @@ class JobApplicationsController < ApplicationController
 		redirect_to applications_job_url(@job_application.job)
 	end
 
-  def reject
+	def reject
 		unless @job_application.active?
-			flash[:alert] = "Invalid action on inactive job application."
-    else
-      @job_application.reason_for_rejection = params[:reason_for_rejection]
-      @job_application.save
+			if request.xhr?
+				render json: {message: "Cannot reject an inactive job application",
+											status: 405}
+			else
+				flash[:alert] = "Cannot reject an inactive job application."
+				redirect_to applications_path(@job_application)
+			end
+		else
+			@job_application.reason_for_rejection = params[:reason_for_rejection]
+			@job_application.save
 			@job_application.reject
-      Event.create(:APP_REJECTED, @job_application) if @job_application.job_seeker.job_developer
-			flash[:info] = "Job application rejected."
+			Event.create(:APP_REJECTED, @job_application) if @job_application.job_seeker.job_developer
+
+			if request.xhr?
+				render json: {message: "Job application rejected", status: 200}
+			else
+				flash[:notice] = "Job application rejected."
+				redirect_to controller: 'jobs', action: 'applications',
+										id: @job_application.job.id
+			end
 		end
-		redirect_to applications_job_url(@job_application.job)
 	end
 
 	def show
@@ -38,5 +50,5 @@ class JobApplicationsController < ApplicationController
 			redirect_back_or_default
 		end
 	end
-  
+
 end
