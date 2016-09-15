5d7f0cf8721894b93016c6fd53240412b1b24b26
Patrick Bolger <patmbolger@gmail.com>
2016-09-15 09:30:35 -0400

    Closes AgileVentures/MetPlus_tracker#419 (#374)
    * converted to cruncher v2
    
    * added concern for cruncher shared methods
    
    * uncommented webmock allow_net_connect
    
    * remove unneccessary comments
    
    * added key_id arg to process_match_results
    
    * changed test score to 5.0 (from 5.5)

--- spec/services/resume_cruncher_spec.rb
+++ spec/services/resume_cruncher_spec.rb
@@ -48,10 +48,17 @@ RSpec.describe ResumeCruncher, type: :model do
 
   describe 'match resumes' do
 
-    it 'returns success' do
-
+    it 'returns array of résumé matches for a valid request' do
       stub_cruncher_match_resumes
-      expect(ResumeCruncher.match_resumes(1)).not_to be nil
+      results = ResumeCruncher.match_resumes(1)
+      expect(results).not_to be nil
+      expect(results.class).to be Array
+      expect(results[0][0]).to be 7
+      expect(results[0][1]).to be 4.9
+      expect(results[1][0]).to be 5
+      expect(results[1][1]).to be 3.8
+      expect(results[2][0]).to be 2
+      expect(results[2][1]).to be 2.0
     end
 
   end
