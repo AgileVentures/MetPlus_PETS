b152601f1ed262b46fc0265ed945e8f6b6408d6d
Joao Pereira <joaopapereira@gmail.com>
2016-09-30 17:02:31 -0400

    Added rescue for pg

--- db/migrate/20160825220149_change_job_status_to_enum.rb
+++ db/migrate/20160825220149_change_job_status_to_enum.rb
@@ -1,9 +1,13 @@
 class ChangeJobStatusToEnum < ActiveRecord::Migration
   def change
-    if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
-      execute "ALTER TABLE jobs ALTER status DROP DEFAULT;"
-      change_column :jobs, :status, 'integer USING CAST(status AS integer)', default: 0, null: false
-    else
+    begin
+      if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+        execute "ALTER TABLE jobs ALTER status DROP DEFAULT;"
+        change_column :jobs, :status, 'integer USING CAST(status AS integer)', default: 0, null: false
+      else
+        change_column :jobs, :status, :integer, default: 0, null: false
+      end
+    rescue NameError
       change_column :jobs, :status, :integer, default: 0, null: false
     end
   end
