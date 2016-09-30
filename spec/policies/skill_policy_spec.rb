4a433797ac007411240335934c0f2ae8c2338d3c
João Pereira <joaopapereira@gmail.com>
2016-09-30 11:11:11 -0400

    Close AgileVentures/MetPlus_tracker#442 (#388)
    * Remove extra file
    
    * Added tests
    
    * Added policies
    
    * Merged all the tests
    
    * Added authentication
    
    * Added authentication
    
    * Added authentication to show method
    
    * Added tests and implementation for update
    
    * Added tests and implementation for delete
    
    * Added before_action to check if user is logged in
    
    * Remove nil check on user
    
    Removed check for nil user as the user will always be logged in
    
    * User not logged in
    
    Change test to check different error when user not logged in
    
    * Removed test no longer used
    
    * Corrected failing tests

---
+++ spec/policies/skill_policy_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe SkillPolicy do
+
+  let(:agency) { FactoryGirl.create(:agency) }
+  let(:company) { FactoryGirl.create(:company) }
+  let(:jd)     { FactoryGirl.create(:job_developer, agency: agency) }
+  let(:cm)     { FactoryGirl.create(:case_manager, agency: agency) }
+  let(:admin)  { FactoryGirl.create(:agency_admin, agency: agency) }
+  let(:company_admin) {FactoryGirl.create(:company_admin, company: company)}
+  let(:company_contact) {FactoryGirl.create(:company_contact, company: company)}
+  let(:skill) {FactoryGirl.create(:skill)}
+
+  permissions :create?, :show?, :update?, :destroy? do
+    it 'denies access if user is case manager' do
+      expect(SkillPolicy).not_to permit(cm, skill)
+    end
+
+    it 'denies access if user is job developer' do
+      expect(SkillPolicy).not_to permit(jd, skill)
+    end
+
+    it 'allows access if user is an agency admin' do
+      expect(SkillPolicy).to permit(admin, skill)
+    end
+
+    it 'denies access if user is company admin' do
+      expect(SkillPolicy).not_to permit(company_admin, skill)
+    end
+
+    it 'denies access if user is company contact' do
+      expect(SkillPolicy).not_to permit(company_contact, skill)
+    end
+  end
+end
