dbd056e411554ed42d791b57606cea2e147c0a6b
Joao Pereira <joaopapereira@gmail.com>
2016-09-30 16:58:04 -0400

    added if for postgre

--- db/migrate/20160825220149_change_job_status_to_enum.rb
+++ db/migrate/20160825220149_change_job_status_to_enum.rb
@@ -1,6 +1,10 @@
 class ChangeJobStatusToEnum < ActiveRecord::Migration
   def change
-    execute "ALTER TABLE jobs ALTER status DROP DEFAULT;"
-    change_column :jobs, :status,  'integer USING CAST(status AS integer)', default: 0, null: false
+    if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+      execute "ALTER TABLE jobs ALTER status DROP DEFAULT;"
+      change_column :jobs, :status, 'integer USING CAST(status AS integer)', default: 0, null: false
+    else
+      change_column :jobs, :status, :integer, default: 0, null: false
+    end
   end
 end
