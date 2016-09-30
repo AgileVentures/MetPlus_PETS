eb24755875e5ccaec9041883c4b0c8915ff4e4fc
João Pereira <joaopapereira@gmail.com>
2016-09-30 11:11:11 -0400

    Close AgileVentures/MetPlus_tracker#442 (#388)
    * Remove extra file
    
    * Added tests
    
    * Added policies
    
    * Merged all the tests
    
    * Added authentication
    
    * Added authentication
    
    * Added authentication to show method
    
    * Added tests and implementation for update
    
    * Added tests and implementation for delete
    
    * Added before_action to check if user is logged in
    
    * Remove nil check on user
    
    Removed check for nil user as the user will always be logged in
    
    * User not logged in
    
    Change test to check different error when user not logged in
    
    * Removed test no longer used
    
    * Corrected failing tests

--- spec/controllers/skills_controller_spec.rb
+++ spec/controllers/skills_controller_spec.rb
@@ -1,67 +1,147 @@
 require 'rails_helper'
 include ServiceStubHelpers::Cruncher
 
+RSpec.shared_examples "unauthorized" do
+  before :each do
+    warden.set_user user
+  end
+  subject{my_request}
+  it 'returns http unauthorized' do
+    expect(subject).to have_http_status(403)
+  end
+  it 'check content' do
+    expect(subject.body).to eq({:message => 'You are not authorized to perform this action.'}.to_json)
+  end
+end
+
+RSpec.shared_examples "unauthorized all" do
+  let(:agency) {FactoryGirl.create(:agency)}
+  let(:company) {FactoryGirl.create(:company)}
+  context "Not logged in" do
+    subject{my_request}
+    it 'returns http unauthorized' do
+      expect(subject).to have_http_status(401)
+    end
+    it 'check content' do
+      expect(subject.body).to eq({:message => 'You need to login to perform this action.'}.to_json)
+    end
+  end
+  context "Case Manager" do
+    it_behaves_like "unauthorized" do
+      let(:user) {FactoryGirl.create(:case_manager, agency: agency)}
+    end
+  end
+  context "Jod Developer" do
+    it_behaves_like "unauthorized" do
+      let(:user) {FactoryGirl.create(:job_developer, agency: agency)}
+    end
+  end
+  context "Job Seeker" do
+    it_behaves_like "unauthorized" do
+      let(:user) {FactoryGirl.create(:job_seeker)}
+    end
+  end
+  context "Company admin" do
+    it_behaves_like "unauthorized" do
+      let(:user) {FactoryGirl.create(:company_admin, company: company)}
+    end
+  end
+  context "Company contact" do
+    it_behaves_like "unauthorized" do
+      let(:user) {FactoryGirl.create(:company_contact, company: company)}
+    end
+  end
+end
+
 RSpec.describe SkillsController, type: :controller do
 
   describe "POST #create" do
+    let(:agency) {FactoryGirl.create(:agency)}
     let(:skill_params) { FactoryGirl.attributes_for(:skill) }
+    context "authorized access" do
+      before :each do
+        aa = FactoryGirl.create(:agency_admin, agency: agency)
+        sign_in aa
+      end
+      it 'creates new skill for valid parameters' do
+        expect { xhr :post, :create, skill: skill_params }.
+          to change(Skill, :count).by(+1)
+      end
+      it 'returns success for valid parameters' do
+        xhr :post, :create, skill: skill_params
+        expect(response).to have_http_status(:success)
+      end
 
-    it 'creates new skill for valid parameters' do
-      expect { xhr :post, :create, skill: skill_params }.
-        to change(Skill, :count).by(+1)
+      it 'returns errors and error status for invalid parameters' do
+        xhr :post, :create, skill: {name: '', description: ''}
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response).to render_template('shared/_error_messages')
+      end
     end
-    it 'returns success for valid parameters' do
-      xhr :post, :create, skill: skill_params
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'returns errors and error status for invalid parameters' do
-      xhr :post, :create, skill: {name: '', description: ''}
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response).to render_template('shared/_error_messages')
+    it_behaves_like "unauthorized all" do
+      let(:my_request) {xhr :post, :create, skill: skill_params}
     end
   end
 
   describe "GET #show" do
+    let(:agency) {FactoryGirl.create(:agency)}
     let(:skill)  { FactoryGirl.create(:skill) }
+    context "authorized access" do
+      before :each do
+        aa = FactoryGirl.create(:agency_admin, agency: agency)
+        sign_in aa
+      end
+      context 'skill found' do
+        before(:each) do
+          xhr :get, :show, id: skill
+        end
 
-    context 'skill found' do
-      before(:each) do
-        xhr :get, :show, id: skill
+        it 'renders json structure' do
+          expect(JSON.parse(response.body))
+              .to match({'id' => skill.id,
+                         'name' => skill.name,
+                         'description' => skill.description})
+        end
+        it "returns http success" do
+          expect(response).to have_http_status(:success)
+        end
       end
 
-      it 'renders json structure' do
-        expect(JSON.parse(response.body))
-            .to match({'id' => skill.id,
-                       'name' => skill.name,
-                       'description' => skill.description})
-      end
-      it "returns http success" do
-        expect(response).to have_http_status(:success)
+      context 'skill NOT found' do
+        it "returns http status not_found" do
+          xhr :get, :show, id: 0
+          expect(response).to have_http_status(:not_found)
+        end
       end
     end
 
-    context 'skill NOT found' do
-      it "returns http status not_found" do
-        xhr :get, :show, id: 0
-        expect(response).to have_http_status(:not_found)
-      end
+    it_behaves_like "unauthorized all" do
+      let(:my_request) {xhr :get, :show, id: skill}
     end
   end
 
   describe "PATCH #update" do
+    let(:agency) {FactoryGirl.create(:agency)}
     let(:skill)  { FactoryGirl.create(:skill) }
     let(:skill_params) { FactoryGirl.attributes_for(:skill) }
+    context "authorized access" do
+      before :each do
+        aa = FactoryGirl.create(:agency_admin, agency: agency)
+        sign_in aa
+      end
+      it 'returns success for valid parameters' do
+        xhr :patch, :update, id: skill, skill: skill_params
+        expect(response).to have_http_status(:success)
+      end
 
-    it 'returns success for valid parameters' do
-      xhr :patch, :update, id: skill, skill: skill_params
-      expect(response).to have_http_status(:success)
+      it 'returns errors and error status for invalid parameters' do
+        xhr :patch, :update, id: skill, skill: {name: '', description: ''}
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response).to render_template('shared/_error_messages')
+      end
     end
-
-    it 'returns errors and error status for invalid parameters' do
-      xhr :patch, :update, id: skill, skill: {name: '', description: ''}
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response).to render_template('shared/_error_messages')
+    it_behaves_like "unauthorized all" do
+      let(:my_request) {xhr :patch, :update, id: skill, skill: skill_params}
     end
   end
 
@@ -72,30 +152,41 @@ RSpec.describe SkillsController, type: :controller do
       stub_cruncher_job_create
     end
 
-    let(:skill)     { FactoryGirl.create(:skill) }
-    let!(:job_skill) { FactoryGirl.create(:job_skill, skill: skill)}
-
-    context 'skill found' do
-
-      it 'deletes skill' do
-        expect { xhr :delete, :destroy, id: skill }.
-            to change(Skill, :count).by(-1)
+    context "authorized access" do
+      let(:agency) {FactoryGirl.create(:agency)}
+      before :each do
+        aa = FactoryGirl.create(:agency_admin, agency: agency)
+        sign_in aa
       end
-      it 'deletes associated job_skill' do
-        expect { xhr :delete, :destroy, id: skill }.
-            to change(JobSkill, :count).by(-1)
+      let(:skill)     { FactoryGirl.create(:skill) }
+      let!(:job_skill) { FactoryGirl.create(:job_skill, skill: skill)}
+
+      context 'skill found' do
+
+        it 'deletes skill' do
+          expect { xhr :delete, :destroy, id: skill }.
+              to change(Skill, :count).by(-1)
+        end
+        it 'deletes associated job_skill' do
+          expect { xhr :delete, :destroy, id: skill }.
+              to change(JobSkill, :count).by(-1)
+        end
+        it "returns http success" do
+          xhr :delete, :destroy, id: skill
+          expect(response).to have_http_status(:success)
+        end
       end
-      it "returns http success" do
-        xhr :delete, :destroy, id: skill
-        expect(response).to have_http_status(:success)
+
+      context 'skill NOT found' do
+        it "returns http status not_found" do
+          xhr :delete, :destroy, id: 0
+          expect(response).to have_http_status(:not_found)
+        end
       end
     end
 
-    context 'skill NOT found' do
-      it "returns http status not_found" do
-        xhr :delete, :destroy, id: 0
-        expect(response).to have_http_status(:not_found)
-      end
+    it_behaves_like "unauthorized all" do
+      let(:my_request) {  xhr :delete, :destroy, id: 0}
     end
   end
 
