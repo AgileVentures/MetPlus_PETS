d5329a4df802a2f235da60f3404542198a775a36
Joao Pereira <joaopapereira@gmail.com>
2016-09-30 16:53:32 -0400

    Added corrections for postgre

--- db/migrate/20160825220149_change_job_status_to_enum.rb
+++ db/migrate/20160825220149_change_job_status_to_enum.rb
@@ -1,5 +1,6 @@
 class ChangeJobStatusToEnum < ActiveRecord::Migration
   def change
-    change_column :jobs, :status, :integer, default: 0, null: false
+    execute "ALTER TABLE jobs ALTER status DROP DEFAULT;"
+    change_column :jobs, :status,  'integer USING CAST(status AS integer)', default: 0, null: false
   end
 end
