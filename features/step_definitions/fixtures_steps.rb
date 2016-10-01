8be059d2154762b3239590ac702ceecb11cdd387
sandhyakoti <sandhyakoti@gmail.com>
2016-10-01 22:43:21 +0530

    Closes AgileVentures/Metplus_tracker#374  (#386)
    * Cherry picked fixes for Metplus_tracker#374
    
    * Incorporated changes of PR#385
    
    * js file errors corrected
    
    * Test case updated
    
    * Merge errors corrected

--- features/step_definitions/fixtures_steps.rb
+++ features/step_definitions/fixtures_steps.rb
@@ -191,7 +191,6 @@ Given(/^the following jobs exist:$/) do |table|
     end
 
     job.save!
-
     if skills and not skills.empty?
       skills.split(/(?:,\s*)/).each do |skill|
         JobSkill.create(job: job, skill: Skill.find_by_name(skill),
