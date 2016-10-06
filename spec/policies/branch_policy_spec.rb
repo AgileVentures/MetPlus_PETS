3db84243091728f069df688af1c44fbf1831699c
vijayakarumudi <vijaya.karumudi@yahoo.com>
2016-10-06 15:20:33 -0300

    added new test

--- spec/policies/branch_policy_spec.rb
+++ spec/policies/branch_policy_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe BranchPolicy do
 
   let(:agency) {FactoryGirl.create(:agency)}
-  let(:branch){ FactoryGirl.create(:branch, agency: agency) }
+  let(:company){FactoryGirl.create(:company)}
+  let(:branch) {FactoryGirl.create(:branch, agency: agency)}
   let(:jd)     {FactoryGirl.create(:job_developer, agency: agency)}
   let(:cm)     {FactoryGirl.create(:case_manager, agency: agency)}
   let(:admin)  {FactoryGirl.create(:agency_admin, agency: agency)}
+  let(:company_admin) {FactoryGirl.create(:company_admin, company: company)}
+  let(:company_contact) {FactoryGirl.create(:company_contact, company: company)}
   
   
 
@@ -24,6 +27,14 @@ RSpec.describe BranchPolicy do
 
     it 'allows access if user is an agency admin' do    
       expect(BranchPolicy).to permit(admin, branch)
+    end
+
+    it 'denies access if user is company admin' do
+      expect(BranchPolicy).not_to permit(company_admin, branch)
+    end
+
+    it 'denies access if user is company contact' do
+      expect(BranchPolicy).not_to permit(company_contact, branch)
     end
 
   end
