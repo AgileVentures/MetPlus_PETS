53b108840b68622029abc00936bff031ac55af06
João Pereira <joaopapereira@gmail.com>
2016-10-12 08:58:23 -0400

    Closes AgileVentures/MetPlus_tracker#334 (#405)
    * Remove extra file
    
    * Remove duplicated information
    Added variable that will be used to solve the problem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Default host for all emails
-  config.action_mailer.default_url_options = { :host => 'localhost', :port => '3000'}
-
   # Do/Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
@@ -49,5 +46,4 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost', :port => '3000'}
 
   config.action_mailer.delivery_method = :mailgun
-
 end
