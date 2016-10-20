e59bf46b6f3181d51a384ccabbf22ee826cbe470
Patrick Bolger <patmbolger@gmail.com>
2016-10-20 08:28:50 -0400

    Closes AgileVentures/MetPlus_tracker#428 (#407)
    * interim progress
    
    * mailgun email model validation
    
    * updated cuke tests with valid email addresses
    
    * removed debug statement
    
    * increased cuke wait duration
    
    * increased cuke step wait duration
    
    * added step after changing browser
    
    * moved public key to init file
    
    * changed driver to selenium
    
    * increase cuke wait step duration
    
    * increased cuke step wait duration
    
    * test srv API not called when email str empty

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,7 +1,24 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-      object.errors[attribute] << (options[:message] || 'is not formatted properly')
+
+    valid_check = EmailValidateService.validate_email(value)
+
+    if valid_check[:status] == 'SUCCESS'
+
+      return if valid_check[:valid]
+
+      if valid_check[:did_you_mean].nil?
+        object.errors[attribute] << (options[:message] || 'is not a valid address')
+      else
+        object.errors[attribute] <<
+              "is not valid (did you mean ... #{valid_check[:did_you_mean]}?)"
+      end
+
+    else
+      # Fall back to format check if service not available
+      unless value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+        object.errors[attribute] << (options[:message] || 'is not formatted properly')
+      end
     end
   end
 end
