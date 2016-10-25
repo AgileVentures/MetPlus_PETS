c12c70f09bfa3f8d198b4ec4e38d27e95f3f519a
Patrick Bolger <patmbolger@gmail.com>
2016-10-25 06:03:55 -0400

    added tests

--- app/models/company.rb
+++ app/models/company.rb
@@ -58,19 +58,6 @@ class Company < ActiveRecord::Base
     company.company_people.joins(:user).order('users.last_name')
   end
 
-  def sole_company_admin?
-    # Is this person even an admin?
-    return false unless company_roles.pluck(:role).include? CompanyRole::ROLE[:CA]
-
-    not other_company_admin?
-  end
-
-  def other_company_admin?
-    admins = Company.company_admins(company)
-
-    (admins.count > 1) || (admins.count == 1 && !admins.include?(self))
-  end
-
   def people_on_role role
     users = []
     company_people.each do |person|
