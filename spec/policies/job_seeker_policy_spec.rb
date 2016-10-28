a47875aff8dcb47903e1d7be273ac559b38a8908
jeanwei <jeanwei938@msn.com>
2016-10-28 12:15:36 +0800

    update js_policy_spec.rb

--- spec/policies/job_seeker_policy_spec.rb
+++ spec/policies/job_seeker_policy_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe JobSeekerPolicy do
   let(:cm2)     { FactoryGirl.create(:case_manager, agency: agency) }
   let(:admin)   { FactoryGirl.create(:agency_admin, agency: agency) }
   let(:cc)      { FactoryGirl.create(:company_contact) }
+  let(:ca)      { FactoryGirl.create(:company_admin) }
 
   permissions :update?, :edit? do
     it "only allows access if user is the account owner" do
@@ -25,6 +26,24 @@ RSpec.describe JobSeekerPolicy do
       expect(JobSeekerPolicy).to permit(jd1, js1)
       expect(JobSeekerPolicy).not_to permit(cm2, js1)
       expect(JobSeekerPolicy).not_to permit(jd2, js1)
+      expect(JobSeekerPolicy).not_to permit(admin, js1)
+      expect(JobSeekerPolicy).not_to permit(cc, js1)
+      expect(JobSeekerPolicy).not_to permit(ca, js1)
+    end
+  end
+
+  permissions :create?, :new? do
+    it "only allows access if user is visitor" do
+      expect(JobSeekerPolicy).to permit(visitor, JobSeeker.new)
+    end
+
+    it "only allows access if user is agency people" do
+      expect(JobSeekerPolicy).to permit(jd1, JobSeeker.new)
+      expect(JobSeekerPolicy).to permit(cm1, JobSeeker.new)
+      expect(JobSeekerPolicy).to permit(admin, JobSeeker.new)
+      expect(JobSeekerPolicy).not_to permit(cc, JobSeeker.new)
+      expect(JobSeekerPolicy).not_to permit(ca, JobSeeker.new)
+      expect(JobSeekerPolicy).not_to permit(js1, JobSeeker.new)
     end
   end
 
@@ -41,6 +60,7 @@ RSpec.describe JobSeekerPolicy do
       expect(JobSeekerPolicy).to permit(cm1, js1)
       expect(JobSeekerPolicy).to permit(admin, js1)
       expect(JobSeekerPolicy).not_to permit(cc, js1)
+      expect(JobSeekerPolicy).not_to permit(ca, js1)
     end
   end
 
@@ -58,6 +78,7 @@ RSpec.describe JobSeekerPolicy do
 
     it "only allow access if user is company people" do
       expect(JobSeekerPolicy).to permit(cc, js1)
+      expect(JobSeekerPolicy).to permit(ca, js1)
     end
   end
 
@@ -68,7 +89,9 @@ RSpec.describe JobSeekerPolicy do
       expect(JobSeekerPolicy).to permit(jd1, js1)
       expect(JobSeekerPolicy).not_to permit(cm1, js1)
       expect(JobSeekerPolicy).not_to permit(jd2, js1)
+      expect(JobSeekerPolicy).not_to permit(admin, js1)
       expect(JobSeekerPolicy).not_to permit(cc, js1)
+      expect(JobSeekerPolicy).not_to permit(ca, js1)
     end
   end
 
@@ -83,6 +106,7 @@ RSpec.describe JobSeekerPolicy do
       expect(JobSeekerPolicy).not_to permit(jd1, js1)
       expect(JobSeekerPolicy).not_to permit(cm1, js1)
       expect(JobSeekerPolicy).not_to permit(cc, js1)
+      expect(JobSeekerPolicy).not_to permit(ca, js1)
     end
   end
 
@@ -98,6 +122,9 @@ RSpec.describe JobSeekerPolicy do
       expect(JobSeekerPolicy).not_to permit(js2, js1)
       expect(JobSeekerPolicy).not_to permit(cm1, js1)
       expect(JobSeekerPolicy).not_to permit(jd1, js1)
+      expect(JobSeekerPolicy).not_to permit(admin, js1)
+      expect(JobSeekerPolicy).not_to permit(ca, js1)
+      expect(JobSeekerPolicy).not_to permit(cc, js1)
     end
   end
 end
