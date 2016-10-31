cc8f6dea688ccc37b6889e6e94fa960edade1f3a
Sandhya <sandhyakoti@gmail.com>
2016-10-31 16:23:56 +0530

    Fixing hound issues

--- app/assets/javascripts/utc_to_local.js
+++ app/assets/javascripts/utc_to_local.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $.each($('.utc_to_local_time'), function (index, value) {
-        var utc_time = moment.tz(value.innerHTML.replace(/ UTC/, ''), 'UTC');
-        $(value).text(moment(utc_time).tz(moment.tz.guess()).format('LLL'));
+        var utcTime = moment.tz(value.innerHTML.replace(/ UTC/, ''), 'UTC');
+        $(value).text(moment(utcTime).tz(moment.tz.guess()).format('LLL'));
     });
 });
