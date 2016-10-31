60c2278f809be028d8cbaa4381dade7b2cce1306
Sandhya <sandhyakoti@gmail.com>
2016-10-31 13:56:03 +0530

    Fixed some violations

--- app/assets/javascripts/utc_to_local.js
+++ app/assets/javascripts/utc_to_local.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $.each($('.utc_to_local_time'), function (index, value) {
-        utc_time = moment.tz(value.innerHTML.replace(/ UTC/, ''), 'UTC')
+        var utc_time = moment.tz(value.innerHTML.replace(/ UTC/, ''), 'UTC');
         $(value).text(moment(utc_time).tz(moment.tz.guess()).format('LLL'));
-    })
+    });
 });
