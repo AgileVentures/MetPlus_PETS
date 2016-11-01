1d3267871f7110ceafc85694c49f406f28c9ece0
vijayakarumudi <vijaya.karumudi@yahoo.com>
2016-11-01 15:46:55 -0300

    Removed extra spaces

--- spec/policies/branch_policy_spec.rb
+++ spec/policies/branch_policy_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
-
 RSpec.describe BranchPolicy do
-
-  let(:agency)  { FactoryGirl.create(:agency)}
-  let(:agency1) { FactoryGirl.create(:agency)}
-  let(:company) { FactoryGirl.create(:company, agencies: [agency])}
+  let(:agency)  { FactoryGirl.create(:agency) }
+  let(:agency1) { FactoryGirl.create(:agency) }
+  let(:company) { FactoryGirl.create(:company, agencies: [agency]) }
   let(:branch)  { FactoryGirl.create(:branch, agency: agency)}
-  let(:jd)      { FactoryGirl.create(:job_developer, agency: agency)}
-  let(:cm)      { FactoryGirl.create(:case_manager, agency: agency)}
-  let(:admin)   { FactoryGirl.create(:agency_admin, agency: agency)}
-  let(:jd1)     { FactoryGirl.create(:job_developer, agency: agency1)}
-  let(:cm1)     { FactoryGirl.create(:case_manager, agency: agency1)}
-  let(:admin1)  { FactoryGirl.create(:agency_admin, agency: agency1)}
-  let(:company_admin)   { FactoryGirl.create(:company_admin, company: company)}
+  let(:jd)      { FactoryGirl.create(:job_developer, agency: agency) }
+  let(:cm)      { FactoryGirl.create(:case_manager, agency: agency) }
+  let(:admin)   { FactoryGirl.create(:agency_admin, agency: agency) }
+  let(:jd1)     { FactoryGirl.create(:job_developer, agency: agency1) } 
+  let(:cm1)     { FactoryGirl.create(:case_manager, agency: agency1) }
+  let(:admin1)  { FactoryGirl.create(:agency_admin, agency: agency1) }
+  let(:company_admin)   { FactoryGirl.create(:company_admin, company: company) }
   let(:company_contact) { FactoryGirl.create(:company_contact, company: company)}
-  let(:js) { FactoryGirl.create(:job_seeker)}
+  let(:js) { FactoryGirl.create(:job_seeker) }
  
   permissions :new?, :create?, :edit?, :update?, :destroy? do
     it "denies access if user is job developer" do
