f1b42ea700a340ada77bbd3cfe7198f57ccd75c8
Joao Pereira <joaopapereira@gmail.com>
2016-11-03 12:51:11 -0400

    Added search icon to the page

--- app/controllers/concerns/job_seekers_viewer.rb
+++ app/controllers/concerns/job_seekers_viewer.rb
@@ -5,32 +5,34 @@ module JobSeekersViewer
     collection = nil
     case people_type
     when 'jobseeker-cm'
-      
-      collection = agency_person.job_seekers_as_case_manager    
+
+      collection = agency_person.job_seekers_as_case_manager
     when 'jobseeker-jd'
-      
+
       collection = agency_person.job_seekers_as_job_developer
-    
+
     when 'jobseeker-without-jd'
 
       collection = JobSeeker.job_seekers_without_job_developer
-                                     
+
     when 'jobseeker-without-cm'
-     
+
       collection = JobSeeker.job_seekers_without_case_manager
 
     end
-    
+
     return collection if collection.nil?
-   
+
     collection.paginate(page: params[:jobseekers_page], per_page: per_page)
 
   end
-  
-  FIELDS_IN_PEOPLE_TYPE = {
-       'jobseeker-cm':[:full_name, :job_seeker_status_short_description,:last_sign_in_at],
 
-       'jobseeker-jd':[:full_name, :job_seeker_status_short_description,:last_sign_in_at],
+  FIELDS_IN_PEOPLE_TYPE = {
+       'jobseeker-cm': [ :full_name, :job_seeker_status_short_description,
+                         :last_sign_in_at, :match_rating ],
+
+       'jobseeker-jd': [ :full_name, :job_seeker_status_short_description,
+                         :last_sign_in_at, :match_rating ],
        'jobseeker-without-jd':
 [:full_name, :job_seeker_status_short_description,:last_sign_in_at],
        'jobseeker-without-cm':
@@ -43,7 +45,7 @@ module JobSeekersViewer
   def job_seeker_fields people_type
       FIELDS_IN_PEOPLE_TYPE[people_type.to_sym] || []
   end
-  
+
   #make helper methods visible to views
   def self.included m
     return unless m < ActionController::Base
@@ -51,4 +53,3 @@ module JobSeekersViewer
     m.helper_method :job_seeker_fields
   end
 end
-
