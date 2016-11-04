8231ac15c8e1cf0f2e71f8d08f55fac1104f3fa1
vijayakarumudi <vijaya.karumudi@yahoo.com>
2016-11-04 20:44:35 -0300

    jobseeker notifications

--- app/helpers/stars_renderer.rb
+++ app/helpers/stars_renderer.rb
@@ -22,7 +22,8 @@ module StarsRenderer
 
   def star_image(value)
     content_tag('i', nil,
-                class: ['fa', star_type(value)], 'aria-hidden': 'true')
+                class: ['fa', star_type(value)], 'aria-hidden': 'true') 
+    
   end
 
   def star_type(value)
