d68108b448ff3e5051d0a6b8a31e95b2b3ccd5d0
vijayakarumudi <vijaya.karumudi@yahoo.com>
2016-11-15 02:53:21 -0400

    rubocup update companypolicy

--- app/policies/company_policy.rb
+++ app/policies/company_policy.rb
@@ -1,8 +1,8 @@
 # CompanyPolicy implementation
 class CompanyPolicy < ApplicationPolicy
   def edit?
-    company_admin?(user, record) or agency_admin?(user, record)
-   end
+    company_admin?(user, record) || agency_admin?(user, record)
+  end
 
   def update?
     edit?
@@ -13,25 +13,24 @@ class CompanyPolicy < ApplicationPolicy
   end
 
   def show?
-     edit?
+    edit?
   end
 
   def list_people?
-    company_admin?(user, record) or agency_admin?(user, record) or user.is_company_person?(record)
+    company_admin?(user, record) || agency_admin?(user, record) || user.is_company_person?(record)
   end
 
   private
-  
-  def company_admin? user, record
-     user.is_a? CompanyPerson and user.is_company_admin? record
-  end
- 
-  def agency_admin? user, record
-    User.is_agency_admin? user and (agency_admin_related_to_company? user,record)
-  end
-  
-  def agency_admin_related_to_company? admin, company
-    admin.agency.companies.include? company
+
+  def company_admin?(user, record)
+    user.is_a?(CompanyPerson) && user.is_company_admin?(record)
   end
 
+  def agency_admin?(user, record)
+    User.is_agency_admin?(user) && (agency_admin_related_to_company? user, record)
+  end
+
+  def agency_admin_related_to_company?(admin, company)
+    admin.agency.companies.include? company
+  end
 end
