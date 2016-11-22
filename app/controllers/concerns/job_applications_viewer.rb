0473237eaacfe8ac37bd04eecafce204c869a613
jeanwei <jeanwei938@msn.com>
2016-11-22 16:23:57 +0800

    update houndci

--- app/controllers/concerns/job_applications_viewer.rb
+++ app/controllers/concerns/job_applications_viewer.rb
@@ -1,41 +1,42 @@
 module JobApplicationsViewer
   extend ActiveSupport::Concern
 
-  def display_job_applications application_type, per_page=10, id
-
+  def display_job_applications(application_type, id, per_page = 10)
     case application_type
     when 'job_seeker'
       if pets_user.is_a?(CompanyPerson)
         return JobApplication.paginate(page: params[:applications_page],
-          :per_page => per_page).where(job_seeker: id).    
-          joins(:job).where('jobs.company_id = ?', pets_user.company_id)
+                                       per_page: per_page).where(job_seeker: id)
+                             .joins(:job).where('jobs.company_id = ?',
+                                                pets_user.company_id)
       end
-      return JobApplication.paginate(page: params[:applications_page], 
-        :per_page => per_page).where(job_seeker: id) 
+      JobApplication.paginate(page: params[:applications_page],
+                              per_page: per_page).where(job_seeker: id)
     when 'job-job-developer'
-      return JobApplication.paginate(page: params[:applications_page],
-        :per_page => per_page).where(job: id, job_seeker_id: AgencyRelation.
-        where(agency_person: pets_user, agency_role_id: 1).
-        select(:job_seeker_id)).includes(:job_seeker).order(:status)
+      JobApplication.paginate(page: params[:applications_page],
+                              per_page: per_page)
+                    .where(job: id, job_seeker_id: AgencyRelation
+        .where(agency_person: pets_user, agency_role_id: 1)
+        .select(:job_seeker_id)).includes(:job_seeker).order(:status)
     when 'job-company-person'
-      return JobApplication.paginate(page: params[:applications_page],
-                 :per_page => per_page).where(job: id).
-                 includes(:job_seeker).order(:status)
+      JobApplication.paginate(page: params[:applications_page],
+                              per_page: per_page).where(job: id)
+                    .includes(:job_seeker).order(:status)
     end
   end
 
   FIELDS_IN_APPLICATION_TYPE = {
-      'job_seeker': [:title, :description, :company, :applied_at, :status],
-      'job-job-developer': [:name, :js_status, :applied_at],
-      'job-company-person': [:name, :js_status, :applied_at, :action]
-  }
+    'job_seeker': [:title, :description, :company, :applied_at, :status],
+    'job-job-developer': [:name, :js_status, :applied_at],
+    'job-company-person': [:name, :js_status, :applied_at, :action]
+  }.freeze
 
-  def application_fields application_type
+  def application_fields(application_type)
     FIELDS_IN_APPLICATION_TYPE[application_type.to_sym] || []
   end
 
   # make helper methods visible to views
-  def self.included m
+  def self.included(m)
     return unless m < ActionController::Base
     m.helper_method :application_fields
   end
