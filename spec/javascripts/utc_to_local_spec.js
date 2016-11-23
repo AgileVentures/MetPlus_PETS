3679cbaa8cb04e503544cc773d2f657519d42609
lollypop27 <lara@adfactory.co.za>
2016-11-23 20:12:29 +0200

    Add var before ele and define var top of page

--- spec/javascripts/utc_to_local_spec.js
+++ spec/javascripts/utc_to_local_spec.js
@@ -1,3 +1,11 @@
+var describe;
+var loadFixtures;
+var spyOn;
+var it;
+var moment;
+var beforeEach;
+var utcToLocal;
+var expect;
   // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 describe('Convert UTC to local Time', function () {
   beforeEach(function () {
@@ -7,35 +15,35 @@ describe('Convert UTC to local Time', function () {
   it('New York', function () {
     spyOn(moment.tz, 'guess').and.returnValue('America/New_York');
     utcToLocal.setup();
-    ele = $('.utc_to_local_time')[0];
+    var ele = $('.utc_to_local_time')[0];
     expect($(ele).text()).toEqual('November 7, 2016 1:04 PM');
   });
 
   it('Johannesburg', function () {
     spyOn(moment.tz, 'guess').and.returnValue('Africa/Johannesburg');
     utcToLocal.setup();
-    ele = $('.utc_to_local_time')[0];
+    var ele = $('.utc_to_local_time')[0];
     expect($(ele).text()).toEqual('November 7, 2016 8:04 PM');
   });
 
   it('London', function () {
     spyOn(moment.tz, 'guess').and.returnValue('Europe/London');
     utcToLocal.setup();
-    ele = $('.utc_to_local_time')[0];
+    var ele = $('.utc_to_local_time')[0];
     expect($(ele).text()).toEqual('November 7, 2016 6:04 PM');
   });
 
   it('Shanghai', function () {
     spyOn(moment.tz, 'guess').and.returnValue('Asia/Shanghai');
     utcToLocal.setup();
-    ele = $('.utc_to_local_time')[0];
+    var ele = $('.utc_to_local_time')[0];
     expect($(ele).text()).toEqual('November 8, 2016 2:04 AM');
   });
 
   it('Hawaii', function () {
     spyOn(moment.tz, 'guess').and.returnValue('US/Hawaii');
     utcToLocal.setup();
-    ele = $('.utc_to_local_time')[0];
+    var ele = $('.utc_to_local_time')[0];
     expect($(ele).text()).toEqual('November 7, 2016 8:04 AM');
   });
 });
