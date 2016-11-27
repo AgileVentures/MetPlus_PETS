8c3e1e4b4a197f13f93aadffb2c0577f5ca1f63d
Patrick Bolger <patmbolger@gmail.com>
2016-11-27 15:47:10 -0500

    Closes AgileVentures/MetPlus_tracker#496
    fix postgresql migrations (#450)

--- db/migrate/20161005183138_change_company_person_status_to_enum.rb
+++ db/migrate/20161005183138_change_company_person_status_to_enum.rb
@@ -2,13 +2,13 @@ class ChangeCompanyPersonStatusToEnum < ActiveRecord::Migration
   def change
     begin
       if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
-        execute "UPDATE TABLE company_people SET status='0' where status='Pending'"
-        execute "UPDATE TABLE company_people SET status='1' where status='Invited'"
-        execute "UPDATE TABLE company_people SET status='2' where status='Active'"
-        execute "UPDATE TABLE company_people SET status='3' where status='Inactive'"
-        execute "UPDATE TABLE company_people SET status='4' where status='Denied'"
-
-        change_column :company_people, :status, 'integer USING CAST(status AS integer)', default: 0, null: false
+        execute "ALTER TABLE company_people ALTER status DROP DEFAULT"
+        execute "UPDATE company_people SET status='0' where status='Pending'"
+        execute "UPDATE company_people SET status='1' where status='Invited'"
+        execute "UPDATE company_people SET status='2' where status='Active'"
+        execute "UPDATE company_people SET status='3' where status='Inactive'"
+        execute "UPDATE company_people SET status='4' where status='Denied'"
+        execute "ALTER TABLE company_people ALTER status TYPE integer USING status::integer"
       else
         change_column :company_people, :status, :integer, default: 0, null: false
       end
