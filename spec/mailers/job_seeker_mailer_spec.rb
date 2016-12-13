2ab59eb56ec32ae5109dff8965e734da119c4e0c
vijayakarumudi <vijaya.karumudi@yahoo.com>
2016-12-13 15:47:11 -0400

    Resolve issues rspec test

--- spec/mailers/job_seeker_mailer_spec.rb
+++ spec/mailers/job_seeker_mailer_spec.rb
@@ -1,8 +1,7 @@
-require "rails_helper"
+require 'rails_helper'
 include ServiceStubHelpers::Cruncher
 
 RSpec.describe JobSeekerMailer, type: :mailer do
-
   describe 'Job developer assigned to job seeker' do
     let(:agency)        { FactoryGirl.create(:agency) }
     let(:job_developer) { FactoryGirl.create(:job_developer, agency: agency) }
@@ -10,16 +9,15 @@ RSpec.describe JobSeekerMailer, type: :mailer do
 
     let(:mail) { JobSeekerMailer.job_developer_assigned(job_seeker, job_developer) }
 
-    it "renders the headers" do
+    it 'renders the headers' do
       expect(mail.subject).to eq 'Job developer assigned'
-      expect(mail.to).to eq(["#{job_seeker.email}"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq([job_seeker.email.to_s])
+      expect(mail.from).to eq(['from@example.com'])
     end
-    it "renders the body" do
+    it 'renders the body' do
       expect(mail).to have_body_text(job_developer.full_name(last_name_first: false))
       expect(mail).to have_body_text("has been assigned to you as your #{agency.name} Job Developer.")
     end
-
   end
 
   describe 'Case manager assigned to job seeker' do
@@ -29,16 +27,15 @@ RSpec.describe JobSeekerMailer, type: :mailer do
 
     let(:mail) { JobSeekerMailer.case_manager_assigned(job_seeker, case_manager) }
 
-    it "renders the headers" do
+    it 'renders the headers' do
       expect(mail.subject).to eq 'Case manager assigned'
-      expect(mail.to).to eq(["#{job_seeker.email}"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq([job_seeker.email.to_s])
+      expect(mail.from).to eq(['from@example.com'])
     end
-    it "renders the body" do
+    it 'renders the body' do
       expect(mail).to have_body_text(case_manager.full_name(last_name_first: false))
       expect(mail).to have_body_text("has been assigned to you as your #{agency.name} Case Manager.")
     end
-
   end
 
   describe 'Job applied by job developer' do
@@ -53,44 +50,46 @@ RSpec.describe JobSeekerMailer, type: :mailer do
       stub_cruncher_job_create
     end
 
-    it "renders the headers" do
+    it 'renders the headers' do
       expect(mail.subject).to eq 'Job applied by job developer'
-      expect(mail.to).to eq(["#{job_seeker.email}"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq([job_seeker.email.to_s])
+      expect(mail.from).to eq(['from@example.com'])
     end
-    it "renders the body" do
+    it 'renders the body' do
       expect(mail).to have_body_text(job_developer.full_name(last_name_first: false))
-      expect(mail).to have_body_text("has submitted an application on your behalf to the job:")
+      expect(mail).to have_body_text('has submitted an application on your behalf to the job:')
     end
-    it "includes link to show job" do
+    it 'includes link to show job' do
       expect(mail).to have_body_text(/#{job_url(id: 1)}/)
     end
-
   end
 
   describe 'Job revoked' do
-    let(:job)           { FactoryGirl.create(:job) }
-    let(:job_seeker) { FactoryGirl.create(:job_seeker)}
+    let(:job) { FactoryGirl.create(:job) }
+    let(:job_seeker) { FactoryGirl.create(:job_seeker) }
+    let!(:resume)      { FactoryGirl.create(:resume, job_seeker: job_seeker) }
+    let!(:application) do
+      job.apply job_seeker
+    end
 
     let(:mail) do
       allow(Pusher).to receive(:trigger)
       stub_cruncher_authenticate
       stub_cruncher_job_create
-      JobSeekerMailer.job_revoked(job_seeker.email, job)
+      JobSeekerMailer.job_revoked(job_seeker, job)
     end
 
-    it "renders the headers" do
+    it 'renders the headers' do
       expect(mail.subject).to eq 'Job revoked'
-      expect(mail.to).to eq(["#{job_seeker.email}"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq([job_seeker.email.to_s])
+      expect(mail.from).to eq(['from@example.com'])
     end
-    it "renders the body" do
-      expect(mail).to have_body_text(/A job \(\n.*#{Regexp.quote(job.title)}.*\n\) You had applied to this job on : #{@jobseeker.application_for_job(@job).created_at}
-
+    it 'renders the body' do
+      expect(mail).to have_body_text(/This is to let you know that you had applied to the job/)
+      expect(mail).to have_body_text(/#{job_url(id: 1)}/)
     end
-    it "includes link to show job" do
+    it 'includes link to show job' do
       expect(mail).to have_body_text(/#{job_url(id: 1)}/)
     end
   end
-
 end
