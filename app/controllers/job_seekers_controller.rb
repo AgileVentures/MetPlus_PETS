8b2b0fa719fd4e0ed5fe677d3acddd25dd1c124e
sigu <sigumagwa@gmail.com>
2017-01-13 00:37:17 +0300

    resolve merge conflict

--- app/controllers/job_seekers_controller.rb
+++ app/controllers/job_seekers_controller.rb
@@ -80,11 +80,6 @@ class JobSeekersController < ApplicationController
 
     if models_saved
       sign_in :user, @jobseeker.user, bypass: true if pets_user == @jobseeker
-<<<<<<< HEAD
-      flash[:notice] = 'Jobseeker was updated successfully.'
-      redirect_to(@jobseeker) && return if(pets_user == @jobseeker.case_manager) ||
-                                          (pets_user == @jobseeker.job_developer)
-=======
       if @jobseeker.user.unconfirmed_email?
         flash[:warning] = 'Please check your inbox to update your email address'
       else
@@ -92,7 +87,6 @@ class JobSeekersController < ApplicationController
       end
       redirect_to(@jobseeker) && return if (pets_user == @jobseeker.case_manager) ||
                                            (pets_user == @jobseeker.job_developer)
->>>>>>> upstream/development
       redirect_to root_path
     else
       @resume = resume
