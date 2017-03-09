5ae096cdf4ecd53325d459c6532227d1c4f78e01
tong wei <jeanwei938@msn.com>
2017-03-10 05:29:54 +0800

    Closes AgileVentures/MetPlus_tracker#575 (#528)
    * add remote pagination to searched_job_list
    
    * remove unused code
    
    * update hound errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,13 @@
 module ApplicationHelper
-
   include StarsRenderer
 
-  def status_desc entity
+  def status_desc(entity)
     # Converts entitie's status to descriptive string suitable for display
     entity.status.titleize
   end
 
   def full_title(page_title = '')
-    base_title = "MetPlus"
+    base_title = 'MetPlus'
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 
@@ -28,18 +27,19 @@ module ApplicationHelper
     end
   end
 
-  def text_modal_button(button_text, target_div_id, btn_class='btn btn-warning btn-xs')
+  def text_modal_button(button_text, target_div_id, btn_class = 'btn btn-warning btn-xs')
     haml_tag('button', button_text, class: btn_class,
-              data: {toggle: 'modal', target: "\##{target_div_id}"} )
+                                    data: { toggle: 'modal',
+                                            target: "\##{target_div_id}" })
   end
 
-  def show_person_path person
+  def show_person_path(person)
     return job_seeker_path person if person.is_job_seeker?
     return agency_person_path person if person.is_a? AgencyPerson
     return company_person_path person if person.is_a? CompanyPerson
   end
 
-  def show_person_home_page_path person
+  def show_person_home_page_path(person)
     return root_path if person.nil?
     return home_job_seeker_path person if person.is_job_seeker?
     return home_company_person_path person if person.is_a? CompanyPerson
@@ -49,5 +49,10 @@ module ApplicationHelper
 
   def show_stars(rating)
     render_stars(rating)
+  end
+
+  def paginate(collection, params = {})
+    will_paginate collection, params.merge(renderer:
+      RemoteLinkPaginationHelper::BootstrapLinkRenderer)
   end
 end
