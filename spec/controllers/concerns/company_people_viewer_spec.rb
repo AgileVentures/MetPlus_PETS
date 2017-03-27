ee81afe84bab4f156e6a8e0618c6301bc06285a7
lollypop27 <lara@adafctory.co.za>
2017-03-27 15:03:52 +0200

    fixes hound issues

--- spec/controllers/concerns/company_people_viewer_spec.rb
+++ spec/controllers/concerns/company_people_viewer_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-class TestConcernCompanyPeopleClass <ApplicationController
+class TestConcernCompanyPeopleClass < ApplicationController
   include CompanyPeopleViewer
 end
 
 RSpec.describe TestConcernCompanyPeopleClass do
-  let(:company)      { FactoryGirl.create(:company) }
-  let(:cmpy_person1) { FactoryGirl.create(:company_contact, company: company) }
-  let(:cmpy_person2) { FactoryGirl.create(:company_contact, company: company) }
-  let(:cmpy_person3) { FactoryGirl.create(:company_contact, company: company) }
-  let (:people_fields)      { TestConcernCompanyPeopleClass::FIELDS_IN_PEOPLE_TYPE }
+  let(:company)        { FactoryGirl.create(:company) }
+  let(:cmpy_person1)   { FactoryGirl.create(:company_contact, company: company) }
+  let(:cmpy_person2)   { FactoryGirl.create(:company_contact, company: company) }
+  let(:cmpy_person3)   { FactoryGirl.create(:company_contact, company: company) }
+  let (:people_fields) { TestConcernCompanyPeopleClass::FIELDS_IN_PEOPLE_TYPE }
 
   describe '#display_company_people' do
     it 'returns all company people for a specified company' do
@@ -19,12 +19,12 @@ RSpec.describe TestConcernCompanyPeopleClass do
 
     it 'returns all fields for company people' do
       expect(subject.company_people_fields('company-all'))
-          .to match_array people_fields['company-all'.to_sym]
+        .to match_array people_fields['company-all'.to_sym]
     end
 
     it 'returns all fields for my company people' do
       expect(subject.company_people_fields('my-company-all'))
-          .to match_array people_fields['my-company-all'.to_sym]
+        .to match_array people_fields['my-company-all'.to_sym]
     end
   end
 end
