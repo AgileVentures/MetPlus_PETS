8b40ce8706fcd2a4a85f80a1a911e99ab085cbab
Paul <pcaston@arach.net.au>
2017-04-04 03:37:06 +0000

    fixed hound ci issues

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe ApplicationPolicy do
   subject { described_class }
   let(:js) { FactoryGirl.create(:job_seeker) }
-  [ [:index?, :not], [:create?, :not], [:new?, :not], [:update?, :not],
-    [:edit?, :not], [:destroy?, :not], [:allow?, ] ].each do |method, not_allow|
+  [[:index?, :not], [:create?, :not], [:new?, :not], [:update?, :not],
+   [:edit?, :not], [:destroy?, :not], [:allow?,] ].each do |method, not_allow|
     permissions method do
       it "does #{not_allow} allow #{method} on application" do
-        if not_allow 
-          expect(ApplicationPolicy).not_to permit()
+        if not_allow
+          expect(ApplicationPolicy).not_to permit
         else
-          expect(ApplicationPolicy).to permit()
+          expect(ApplicationPolicy).to permit
         end
       end
     end
