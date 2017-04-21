6532e6834c77ed5d2e497b89bc21c02b553a38b4
Paul <pcaston@arach.net.au>
2017-04-21 04:43:39 +0000

    prior to full regression test

--- spec/controllers/users/confirmations_controller_spec.rb
+++ spec/controllers/users/confirmations_controller_spec.rb
@@ -2,15 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Users::ConfirmationsController, type: :controller do
 	describe "get confirmation_token" do
-	  before(:each) do
+      let!(:agency)    { FactoryGirl.create(:agency) }
+      let!(:aa_person) { FactoryGirl.create(:agency_admin, agency: agency) }
+      let!(:cm_person) { FactoryGirl.create(:case_manager, agency: agency) }
+      let!(:jd_person) { FactoryGirl.create(:job_developer, agency: agency) }
+      before(:each) do
   	    @request.env["devise.mapping"] = Devise.mappings[:user]
   	  end
-      @js = FactoryGirl.create(:user_applicant)
-      @js.send_confirmation_instructions
-      @user_token = @js.confirmation_token
-	  it 'confirms user login' do
+  	  it 'rejects invalid tag' do
+        get :show, confirmation_token: 'HzDZWwMxswSAs_aQSYwd'
+        expect(response).to render_template('users/confirmations/new')
+      end
+      it 'confirms user email address' do
+        @js = FactoryGirl.create(:job_seeker_applicant)
+        get :show, confirmation_token: @js.confirmation_token
+        expect(response).to render_template('/')
+        expect(flash[:notice]).to eq "Your email address has been successfully confirmed."
+      end
+       it 'allows re-confirmation of user email address' do
+        @js = FactoryGirl.create(:job_seeker_applicant)
+        @user_token = @js.confirmation_token
+        get :show, confirmation_token: @js.confirmation_token
         get :show, confirmation_token: @user_token
-        expect(response).to render_template(:login_path)
-       end
+        expect(flash[:notice]).to eq "Your email address has been successfully confirmed."
+      end
     end
 end
--- spec/factories/job_seekers.rb
+++ spec/factories/job_seekers.rb
@@ -5,4 +5,10 @@ FactoryGirl.define do
     association :address, factory: :address
     user
   end
+  factory :job_seeker_applicant, class: JobSeeker do
+    year_of_birth "1998"
+    job_seeker_status
+    association :address, factory: :address
+    association :user, factory: :user_applicant
+  end
 end
