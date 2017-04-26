41451c709c8e87950f1ee7d6147ad6958c16852b
Paul <pcaston@arach.net.au>
2017-04-26 04:56:24 +0000

    changed test to be order independent

--- spec/controllers/company_registrations_controller_spec.rb
+++ spec/controllers/company_registrations_controller_spec.rb
@@ -260,10 +260,8 @@ RSpec.describe CompanyRegistrationsController, type: :controller do
       end
 
       it 'sets User roles to CC and CA' do
-        expect(assigns(:company).company_people[0].company_roles[0].role)
-          .to eq 'Company Contact'
-        expect(assigns(:company).company_people[0].company_roles[1].role)
-          .to eq 'Company Admin'
+        expect(assigns(:company).company_people[0].company_roles[0, 2].map{|x| x[:role]})
+          .to contain_exactly 'Company Contact', 'Company Admin'
       end
 
       it 'sets company status to Pending' do
