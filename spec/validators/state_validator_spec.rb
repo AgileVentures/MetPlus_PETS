715971dabc03de58ae192df91576a47e06497fe5
Paul <pcaston@arach.net.au>
2017-04-28 05:01:08 +0000

    dryed out validator specs'

--- spec/validators/state_validator_spec.rb
+++ spec/validators/state_validator_spec.rb
@@ -1,37 +1,28 @@
 require 'rails_helper'
+require 'support/validator_helper'
+include ValidatorHelper
 
 RSpec.describe StateValidator do
-  let(:model) do
-    Class.new do
-      include ActiveModel::Validations
-
-      attr_accessor :test_State
-
-      def self.model_name
-        ActiveModel::Name.new(self, nil, 'TestModel')
-      end
-    end
-  end
-
+  let(:model) { ValidatorHelper::test_model_class } 
   let(:record) { model.new }
 
   before(:each) do
     record.class_eval do
-      validates :test_State, State: true
+      validates :test_attr, State: true
     end
   end
 
   it 'adds an error when the State is invalid' do
-    record.test_State = 'MX'
+    record.test_attr = 'MX'
     expect do
       record.valid?
     end.to change(record.errors, :messages).from({}).to a_hash_including(
-      test_State: ['not present in the list of states']
+      test_attr: ['not present in the list of states']
     )
   end
 
   it 'does not add an error when the State is valid' do
-    record.test_State = 'MA'
+    record.test_attr = 'MA'
     expect do
       record.valid?
     end.not_to change(record.errors.messages, :count).from(0)
--- spec/validators/url_validator_spec.rb
+++ spec/validators/url_validator_spec.rb
@@ -1,37 +1,28 @@
 require 'rails_helper'
+require 'support/validator_helper'
+include ValidatorHelper
 
 RSpec.describe UrlValidator do
-  let(:model) do
-    Class.new do
-      include ActiveModel::Validations
-
-      attr_accessor :test_url
-
-      def self.model_name
-        ActiveModel::Name.new(self, nil, 'TestModel')
-      end
-    end
-  end
-
+  let(:model) { ValidatorHelper::test_model_class } 
   let(:record) { model.new }
 
   before(:each) do
     record.class_eval do
-      validates :test_url, url: true
+      validates :test_attr, url: true
     end
   end
 
   it 'adds an error when the Url is invalid' do
-    record.test_url = 'htps://ide.c9.io/pcaston/metsplus'
+    record.test_attr = 'htps://ide.c9.io/pcaston/metsplus'
     expect do
       record.valid?
     end.to change(record.errors, :messages).from({}).to a_hash_including(
-      test_url: ['is not an url']
+      test_attr: ['is not an url']
     )
   end
 
   it 'does not add an error when the Url is valid' do
-    record.test_url = 'https://ide.c9.io/pcaston/metsplus'
+    record.test_attr = 'https://ide.c9.io/pcaston/metsplus'
     expect do
       record.valid?
     end.not_to change(record.errors.messages, :count).from(0)
