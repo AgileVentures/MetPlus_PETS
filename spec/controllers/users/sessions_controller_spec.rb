fa400447cbfc1682495cb94f19f143657e288c26
Paul <pcaston@arach.net.au>
2017-05-28 11:50:58 +0000

    changes to session_controller_spec.rb

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 describe Devise::SessionsController, type: :controller do
-
-	it "should route '/login' correctly" do
-		expect(:get => 'login').to route_to(controller: "users/sessions", action: 'new')
-	end
-
-	it "should route '/logout' correctly" do
-		expect(:delete => 'logout').to route_to(controller: "users/sessions", action: 'destroy')
-	end
-
+  describe 'user logs in with cookies' do
+    before_action :user_logged!
+    include UserParameters
+    cookies[:user_id]     = current_user.id
+    cookies[:person_type] = current_user.actable_type
+    expect(:get => 'login').to route_to(controller: "users/sessions", action: 'new')
+  end
+  describe 'user logs in' do
+	  it "should route '/login' correctly" do
+		  expect(:get => 'login').to route_to(controller: "users/sessions", action: 'new')
+	  end
+  end
+  describe 'user logs out' do
+	  it "should route '/logout' correctly" do
+		  expect(:delete => 'logout').to route_to(controller: "users/sessions", action: 'destroy')
+	  end
+  end
 end
