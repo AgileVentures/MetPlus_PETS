b1471ddd3856c11fa380e7da39dc910da97b3e1c
Hinesh Patel <hineshpatel0@googlemail.com>
2017-08-02 20:12:28 +0100

    Fix hound errors

--- db/migrate/20170801191739_add_years_of_experiece_to_jobs.rb
+++ db/migrate/20170801191739_add_years_of_experiece_to_jobs.rb
@@ -1,5 +1,5 @@
 class AddYearsOfExperieceToJobs < ActiveRecord::Migration
   def change
-  	add_column :jobs, :years_of_experience, :integer, null: :true
+    add_column :jobs, :years_of_experience, :integer, null: :true
   end
 end
