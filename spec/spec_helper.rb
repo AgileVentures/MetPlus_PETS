577cee9e0d91ec6661ae65fbe7943fafe96f5448
Patrick Bolger <patmbolger@gmail.com>
2017-10-03 13:52:40 -0400

    reenable non-stubbed requests (otherwise CI integration breaks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,7 +33,7 @@ include ServiceStubHelpers::RecaptchaValidator
 # WebMock.disable!
 
 #  This allows non-mocked external service calls to proceed:
-#WebMock.allow_net_connect!
+WebMock.allow_net_connect!
 
 CodeClimate::TestReporter.start
 RSpec.configure do |config|
