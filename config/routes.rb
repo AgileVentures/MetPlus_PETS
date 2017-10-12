20968b897a62ca1a02f1a2b176afaabaf123dd41
Arun Kumar <arunkumarm1995@gmail.com>
2017-10-12 01:46:24 -0500

    Remove to option which had a symbol as its value

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
     member do
       get :home
       get :edit_profile, to: 'agency_people#edit_profile'
-      get :my_profile, to: :my_profile
+      get :my_profile
       patch :update_profile, to: 'agency_people#update_profile'
       patch 'assign_job_seeker/:job_seeker_id/:agency_role',
             to: 'agency_people#assign_job_seeker',
