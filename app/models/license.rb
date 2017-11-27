1992832d9dd59de8617c9713fa3742f2de7bf5ef
Patrick Bolger <patmbolger@gmail.com>
2017-11-27 16:17:08 -0500

    hound issue

--- app/models/license.rb
+++ app/models/license.rb
@@ -5,7 +5,7 @@ class License < ActiveRecord::Base
   validates_presence_of   :abbr
   validates_uniqueness_of :abbr, case_sensitive: false
 
-  after_validation { self.abbr&.upcase! }
+  after_validation { abbr&.upcase! }
 
   validates_presence_of :title
 
