5c3b077cb0fe527fad8b99da775888e17c4da2ac
Joao Pereira <joaopapereira@gmail.com>
2017-12-08 08:08:16 -0500

    Assign tasks when processing

--- app/interactors/job_applications/processing.rb
+++ app/interactors/job_applications/processing.rb
@@ -1,10 +1,16 @@
 module JobApplications
   class Processing
-    def call(job_application)
-      raise JobNotActive, "Job is not active" unless job_application.active?
+    def call(job_application, company_person)
+      raise JobNotActive, 'Job is not active' unless job_application.active?
       job_application.process
       job_developer = job_application.job_seeker.job_developer
       Event.create(:APP_PROCESSING, job_application) if job_developer
+
+      task = Task.find_by_type_and_target_job_application_open(
+        :job_application,
+        job_application
+      )
+      task.first.assign(company_person) if task.count == 1
     end
   end
 
--- spec/interactors/job_applications/processing_spec.rb
+++ spec/interactors/job_applications/processing_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 include ServiceStubHelpers::Cruncher
 
+class TestTaskHelper
+  include TaskManager::BusinessLogic
+  include TaskManager::TaskManager
+end
+
 RSpec.describe JobApplications::Processing do
-  let!(:agency)       { FactoryGirl.create(:agency) }
-  let(:job_developer) { FactoryGirl.create(:job_developer, agency: agency) }
-  let(:case_manager)  { FactoryGirl.create(:case_manager, agency: agency) }
-  let(:job_seeker)    { FactoryGirl.create(:job_seeker) }
-  let(:service)       { JobSeekers::AssignAgencyPerson.new }
+  let!(:agency)        { FactoryGirl.build(:agency) }
+  let(:job_developer)  { FactoryGirl.build(:job_developer, agency: agency) }
+  let(:company)        { FactoryGirl.build(:company) }
+  let(:company_person) { FactoryGirl.create(:company_person, company: company) }
+  let(:job_seeker)     { FactoryGirl.build(:job_seeker) }
+  let(:service)        { JobSeekers::AssignAgencyPerson.new }
 
   describe 'call' do
     before(:each) do
@@ -19,7 +25,7 @@ RSpec.describe JobApplications::Processing do
       it 'throws an exception' do
         job_application = FactoryGirl.build(:job_application, status: :processing)
         expect do
-          subject.call(job_application)
+          subject.call(job_application, company_person)
         end.to raise_error(JobApplications::JobNotActive)
       end
     end
@@ -31,29 +37,38 @@ RSpec.describe JobApplications::Processing do
       end
 
       it 'change the status of the application to processing' do
-        subject.call(job_application)
-        expect(job_application.processing?).to be(true)        
+        subject.call(job_application, company_person)
+        expect(job_application.processing?).to be(true)
       end
-      
-      context 'when the job seeker as a job developer assigned' do    
+
+      it 'assigns the job application review to the company person' do
+        TestTaskHelper.new_review_job_application_task(job_application, company)
+        subject.call(job_application, company_person)
+        task = Task.find_by_owner_user_open(company_person)
+        expect(task.count).to be(1)
+        expect(task.first.task_type).to eq('job_application')
+        expect(task.first.status).to eq(Task::STATUS[:ASSIGNED])
+      end
+
+      context 'when the job seeker as a job developer assigned' do
         let(:job_developer) { FactoryGirl.build(:job_developer) }
         before(:each) do
           allow(job_seeker).to receive(:job_developer).and_return(job_developer)
-          subject.call(job_application)
+          subject.call(job_application, company_person)
         end
 
         it 'create Job Application processing event' do
           expect(Event).to have_received(:create)
-          .with(
-            :APP_PROCESSING,
-            job_application
-          )
+            .with(
+              :APP_PROCESSING,
+              job_application
+            )
         end
       end
-      
+
       context 'when the job seeker do not have a job developer assigned' do
         before(:each) do
-          subject.call(job_application)
+          subject.call(job_application, company_person)
         end
 
         it 'create Job Application processing event' do
