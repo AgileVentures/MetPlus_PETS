f67124dab44701a5a7e9d1f078d74b9779732d78
Joao Pereira <joaopapereira@gmail.com>
2018-08-28 20:02:47 -0400

    Add a better validation for year of birth
    In the previous code the error message was to generic, it was updated to
    specify the year range that is valid.
    Corrected the seeds file that generated random numbers between 0-100
    starting on 2016, now uses the current year as a base for the calculation
    and generates number between 16-65
    
    Fixes AgileVentures/MetPlus_tracker#721

--- app/validators/year_of_birth_validator.rb
+++ app/validators/year_of_birth_validator.rb
@@ -3,9 +3,9 @@ class YearOfBirthValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     return if value == nil
     reg = /\A\d{4}\z/
-  
-    if !value=~reg  || !((100.years.ago.year..Date.today.year).member?(value.to_i))
-        object.errors[attribute] << (options[:message] || 'incorrect format')
+
+    if !value=~reg || !((100.years.ago.year..Date.today.year - 16).member?(value.to_i))
+        object.errors[attribute] << (options[:message] || 'age need to be between 16 and 100')
     end
   end
 end
