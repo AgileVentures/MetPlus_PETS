c7974753b06b050ef9bcef1e83c2e580c108b730
faithngetich <faithngetich188@gmail.com>
2018-10-30 21:00:31 +0300

    Fix issues highlited by the houndci analyzer

--- app/policies/agency_policy.rb
+++ app/policies/agency_policy.rb
@@ -2,6 +2,7 @@ class AgencyPolicy < ApplicationPolicy
   def update?
     record ? user.agency_admin?(record) : false
   end
+
   def update_job_properties?
     record ? user.job_developer?(record) : false
   end
