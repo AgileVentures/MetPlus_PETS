dffb4bec2a59b08741039d6deab6264476cc692f
Joao Pereira <joaopapereira@gmail.com>
2019-02-15 18:58:57 -0500

    Bump jquery version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-//= require jquery2
+//= require jquery3
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
