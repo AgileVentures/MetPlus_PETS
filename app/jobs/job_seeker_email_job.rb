636dab4c330c5f440d2c656450062384f9e39056
Joao Pereira <joaopapereira@gmail.com>
2019-04-13 10:25:26 -0400

    Revert "Correct rubocop issues on jobs"
    This reverts commit a4d87e6c191e49ad0ca49281e11134db9fe0848a.

--- app/jobs/job_seeker_email_job.rb
+++ app/jobs/job_seeker_email_job.rb
@@ -1,21 +1,22 @@
 class JobSeekerEmailJob < ApplicationJob
   queue_as :default
-  @available_events = {
-    Event::EVT_TYPE[:JD_ASSIGNED_JS] => 'JobSeekerMailer.job_developer_assigned',
-    Event::EVT_TYPE[:JD_SELF_ASSIGN_JS] => 'JobSeekerMailer.job_developer_assigned',
-    Event::EVT_TYPE[:CM_ASSIGNED_JS] => 'JobSeekerMailer.case_manager_assigned',
-    Event::EVT_TYPE[:CM_SELF_ASSIGN_JS] => 'JobSeekerMailer.case_manager_assigned',
-    Event::EVT_TYPE[:JOB_REVOKED] => 'JobSeekerMailer.job_revoked'
-  }
 
   def perform(evt_type, job_seeker, *args)
-    if evt_type == Event::EVT_TYPE[:JD_APPLY]
-      # args[0] = job_developer, args[1] = job
-      JobSeekerMailer.job_applied_by_job_developer(job_seeker, args[0], args[1])
-                     .deliver_later
-    end
-    return unless @available_events.key?(evt_type)
+    case evt_type
+    when Event::EVT_TYPE[:JD_ASSIGNED_JS], Event::EVT_TYPE[:JD_SELF_ASSIGN_JS]
+      # args[0] = agency_person
+      JobSeekerMailer.job_developer_assigned(job_seeker, args[0]).deliver_later
 
-    send(@available_events[evt_type], email_addresses, evt_obj[0]).deliver_later
+    when Event::EVT_TYPE[:CM_ASSIGNED_JS], Event::EVT_TYPE[:CM_SELF_ASSIGN_JS]
+      # args[0] = agency_person
+      JobSeekerMailer.case_manager_assigned(job_seeker, args[0]).deliver_later
+
+    when Event::EVT_TYPE[:JD_APPLY]
+      # args[0] = job_developer, args[1] = job
+      JobSeekerMailer.job_applied_by_job_developer(job_seeker, args[0], args[1]).deliver_later
+
+    when Event::EVT_TYPE[:JOB_REVOKED]
+      JobSeekerMailer.job_revoked(job_seeker, args[0]).deliver_later
+    end
   end
 end
--- app/jobs/notify_email_job.rb
+++ app/jobs/notify_email_job.rb
@@ -1,29 +1,45 @@
 class NotifyEmailJob < ApplicationJob
   queue_as :default
-  @available_events = {
-    Event::EVT_TYPE[:JS_REGISTER] => 'AgencyMailer.job_seeker_registered',
-    Event::EVT_TYPE[:COMP_REGISTER] => 'AgencyMailer.company_registered',
-    Event::EVT_TYPE[:JS_APPLY] => 'AgencyMailer.job_seeker_applied',
-    Event::EVT_TYPE[:APP_ACCEPTED] => 'AgencyMailer.job_application_accepted',
-    Event::EVT_TYPE[:APP_REJECTED] => 'AgencyMailer.job_application_rejected',
-    Event::EVT_TYPE[:APP_PROCESSING] => 'AgencyMailer.job_application_processing',
-    Event::EVT_TYPE[:JD_ASSIGNED_JS] => 'AgencyMailer.job_seeker_assigned_jd',
-    Event::EVT_TYPE[:CM_ASSIGNED_JS] => 'AgencyMailer.job_seeker_assigned_cm',
-    Event::EVT_TYPE[:JOB_POSTED] => 'AgencyMailer.job_posted',
-    Event::EVT_TYPE[:JOB_REVOKED] => 'AgencyMailer.job_revoked'
-  }
 
   def perform(email_addresses, evt_type, *evt_obj)
-    if evt_type == Event::EVT_TYPE[:CP_INTEREST_IN_JS]
+    case evt_type
+    when Event::EVT_TYPE[:JS_REGISTER]
+      AgencyMailer.job_seeker_registered(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:COMP_REGISTER]
+      AgencyMailer.company_registered(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:JS_APPLY]
+      AgencyMailer.job_seeker_applied(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:APP_ACCEPTED]
+      AgencyMailer.job_application_accepted(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:APP_REJECTED]
+      AgencyMailer.job_application_rejected(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:APP_PROCESSING]
+      AgencyMailer.job_application_processing(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:JD_ASSIGNED_JS]
+      AgencyMailer.job_seeker_assigned_jd(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:CM_ASSIGNED_JS]
+      AgencyMailer.job_seeker_assigned_cm(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:JOB_POSTED]
+      AgencyMailer.job_posted(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:JOB_REVOKED]
+      AgencyMailer.job_revoked(email_addresses, evt_obj[0]).deliver_later
+
+    when Event::EVT_TYPE[:CP_INTEREST_IN_JS]
       # evt_obj[0] == company_person
       # evt_obj[1] == job_seeker
       # evt_obj[2] == job
       AgencyMailer.company_interest_in_job_seeker(email_addresses, evt_obj[0],
                                                   evt_obj[1],
                                                   evt_obj[2]).deliver_later
-      return
     end
-
-    send(@available_events[evt_type], email_addresses, evt_obj[0]).deliver_later
   end
 end
